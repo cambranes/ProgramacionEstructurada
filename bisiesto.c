5e4d582e91504914d4cd0ddc19cb1c7b8ba2c524
edgar.cambranes <edgar.cambranes@gmail.com>
2017-01-26 18:36:01 -0600

    update UTF-8

--- bisiesto.c
+++ bisiesto.c
@@ -4,7 +4,7 @@ Entradas:Ano (int)
 Salidas:Impresion
 Procedimiento general:
 Determinar si dado una entrada entera
-representa un anioo bisiesto o no.
+representa un anio bisiesto o no.
 Se determina inicialmente si es multiplo de 4. 
 Dentro de los multiplos de 100 debe cumplir si es multiplo de 400.
 Se utiliza una bandera (esBisiesto)
@@ -21,20 +21,20 @@ int main(int argc, char *argv[])
 	*/
 	int bisiesto, esBisiesto = 0;
 	scanf("%d", &bisiesto);
-	/* Verificacion de mœltiplo de 4 
+	/* Verificacion de multiplo de 4 
 	sino esBisiesto tiene valor por defecto falso*/
 	if (bisiesto%4 == 0)
 	{
-		/* Verificaci—n de mœltiplo de 100*/
+		/* Verificacion de multiplo de 100*/
 		if (bisiesto%100 == 0)
 		{
 			if (bisiesto%400 == 0)
 			{
-				/* Es mœltiplo de 100 y 400*/
+				/* Es multiplo de 100 y 400*/
 				esBisiesto = 1;
 			}			
 		}
-		/* Es mœltiplo de 4 pero no de 100*/
+		/* Es multiplo de 4 pero no de 100*/
 		else
 		{
 		esBisiesto = 1;	
@@ -44,19 +44,15 @@ int main(int argc, char *argv[])
 	/* Salida*/
 	if (esBisiesto)
 	{
-		printf("%d es un a–o bisiesto", bisiesto);
+		printf("%d es bisiesto", bisiesto);
 	}
 	else 
 	{
-<<<<<<< Updated upstream
-		printf("%d NO es un a–o bisiesto", bisiesto);
+		printf("%d NO es bisiesto", bisiesto);
 	}
 	
 	return 0;
 }
 
-=======
-		printf("%d NO es un a�o bisiesto", bisiesto);
-	}	
-	return 0;}
->>>>>>> Stashed changes
+
+
