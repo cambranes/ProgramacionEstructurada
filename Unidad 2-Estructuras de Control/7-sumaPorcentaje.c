b0d3aa78abfac68c122e12b2e788081f7547b269
Bricktor <35347993+Bricktor@users.noreply.github.com>
2018-01-31 11:46:13 -0600

    Add files via upload

---
+++ Unidad 2-Estructuras de Control/7-sumaPorcentaje.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char *argv[]) {
+	float numero, numerofinal;
+	numero=0; numerofinal=0;
+	printf("Capturar el numero:\n");
+	scanf("%f", &numero);
+	numerofinal=numero;
+	if (numero>1000)
+	{
+		numerofinal=numero*1.05;
+		if (numero>3000)
+		{
+			numerofinal=numerofinal+numero*.1;
+			if (numero>5000)
+			{
+				numerofinal=numerofinal+numero*.05;
+			}
+		}
+	}
+	printf("El numero final es %f", numerofinal);
+	return 0;
+}
+
