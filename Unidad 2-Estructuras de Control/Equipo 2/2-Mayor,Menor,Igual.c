91c42fa0c69d24034bd0c1b94a61f85a2a9b891e
Jorge Anzures <35499177+inki189@users.noreply.github.com>
2018-01-31 10:55:13 -0600

    Add files via upload

---
+++ Unidad 2-Estructuras de Control/Equipo 2/2-Mayor,Menor,Igual.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/*Ejercicio 2
+autor:Carlos Alvarez
+Descripcion general:Algortimo para determinar si un numero es mayor,menor o igual con respecto a otro*/
+int main ()
+{
+	/*Se definen las veriables*/
+	int numero,numerodos;
+	/*entrada:Se solicita ingrese el valor para la variable*/
+	printf("introduzca un numero positivo o negativo:" );
+	scanf("%d",&numero);
+	printf("introduzca un numero positivo o negativo: " );
+	scanf("%d",&numerodos);
+	/*proceso:El valor es evaluado en cada una de las condiciones para determinar si es mayor,menor o igual que otro numero*/
+	if(numero> numerodos)
+	{
+		printf("es mayor que %d\n",numero);
+	}
+	else if(numerodos>numero)
+	{
+		printf("es mayor que %d\n",numerodos);
+	}
+	else
+	{
+		printf("son iguales");
+	}
+	/*salida:Se muestra en pantalla los valores obtenidos con su respectiva comparacion*/
+	return 0;
+}
+
