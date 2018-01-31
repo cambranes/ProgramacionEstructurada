167fc4a8a0c18e228902d259dce47180a16573ae
Bricktor <35347993+Bricktor@users.noreply.github.com>
2018-01-31 11:45:30 -0600

    Add files via upload

---
+++ 31-numeroPrimo.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	int numero, i, divisores;
+	divisores=0;
+	printf("Introducir el numero: \n");
+	scanf("%d", &numero);
+	for (i=1;i<=numero;i++)
+	{
+		if(numero%i==0)
+		{
+			divisores++;
+		}
+	}
+	if (divisores==2)
+	{
+		printf("El numero %d es primo.", numero);
+	}
+	else
+		{
+		printf("El número %d no es primo.", numero);
+    	}
+
+	return 0;
+}
+
