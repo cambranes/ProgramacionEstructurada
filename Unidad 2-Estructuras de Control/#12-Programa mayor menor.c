68707218cec489db11bfbd33c5cfbcbf9497402d
kristianzoom3 <35499353+kristianzoom3@users.noreply.github.com>
2018-02-01 11:23:27 -0600

    Update and rename #12-Escribir un programa que lea 2 numeros e imprima el mayor y el menor.c to #12-Programa mayor menor.c

--- Unidad 2-Estructuras de Control/#12-Programa mayor menor.c
+++ Unidad 2-Estructuras de Control/#12-Programa mayor menor.c
@@ -1,4 +1,8 @@
-
+/*#12. Escribir un programa que lea 2 numeros e imprima el mayor y el menor.c
+Autor:Kristian Pantoja
+Entradas: #Reales
+Salida: #Reales
+Simple comparacion Si A<B, Sino A>B, con Si A=B */
 #include<stdio.h>
 
 int main() {
