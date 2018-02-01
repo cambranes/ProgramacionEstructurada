e77de14ae83b6a5eda4202c045b6e759e42f47b2
kristianzoom3 <35499353+kristianzoom3@users.noreply.github.com>
2018-02-01 11:16:16 -0600

    Update #42-Escribir un programa que calcule las raices cuadradas de los primeros 100 numeros enteros y Escribir en 2 columnas diferentes el numero natural y su raiz cuadrada respectivamente.c

--- Unidad 2-Estructuras de Control/#42-Escribir un programa que calcule las raices cuadradas de los primeros 100 numeros enteros y Escribir en 2 columnas diferentes el numero natural y su raiz cuadrada respectivamente.c
+++ Unidad 2-Estructuras de Control/#42-Escribir un programa que calcule las raices cuadradas de los primeros 100 numeros enteros y Escribir en 2 columnas diferentes el numero natural y su raiz cuadrada respectivamente.c
@@ -3,14 +3,19 @@
 #include<stdbool.h>
 
 int main() {
+	/*Entradas*/
 	int a;
 	int b;
 	float c;
 	bool x;
+	
+	/*Procedimiento*/
 	printf("Poner un valor mayor que 0\n");
 	b = 100;
 	scanf("%i",&a);
 	c = (sqrtf(a));
+	
+	/*Salidas*/
 	if (a<=b) {
 		x = sqrtf(a)==(c);
 	}
