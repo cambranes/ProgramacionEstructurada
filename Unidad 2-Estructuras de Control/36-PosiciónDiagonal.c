967c2abd5d71c789472ff9087a8dce83d7dc40ef
EstebanMadrazo <sonicxthewerehog@live.com.mx>
2018-01-31 18:06:24 -0600

    ejercicio 36

---
+++ Unidad 2-Estructuras de Control/36-PosiciónDiagonal.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/* 36.Dado un número N entero positivo, considera que representa una tabla con
+n x n celdas. La numeración de las posiciones inician en 0. Escribe un
+programa que imprima las posiciones de las celdas de la diagonal superior
+hacia abajo.
+N= 5
+00
+10 11
+20 21 22
+30 31 32 33
+40 41 42 43 44
+*/
+int main() {
+	/* Entradas */
+	int celdas=0;
+	int fila;
+	int columna;
+	printf("ingrese el numero de celdas: ");
+	scanf("%i",&celdas);
+	for(int fila=0;fila<celdas;fila++){
+		for(int columna=0;columna<celdas;columna++){
+			if(fila>=columna){
+				printf("%i%i ",fila,columna);
+			} else{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	 }
+	return 0;
+}
+
