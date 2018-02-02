cd2154960a76b98d290cda44cd21afc4fd04255f
Bricktor <35347993+Bricktor@users.noreply.github.com>
2018-02-02 08:05:18 -0600

    Add files via upload

--- Unidad 2-Estructuras de Control/31-numeroPrimo.c
+++ Unidad 2-Estructuras de Control/31-numeroPrimo.c
@@ -1,10 +1,20 @@
+/* Autor:Víctor Ortiz García (30/Enero/18 - 8:00 AM)
+Entrada: numero
+Salida: Divisores
+Procedimiento General: Lee un número, y se hace un ciclo for que se repite las mismas veces que el
+valor del número, y si el número tiene sólamente 2 divisores, es primo. En cualquier otro caso, no es primo.
+*/
+
 #include <stdio.h>
 
+//Entrada
 int main(int argc, char *argv[]) {
 	int numero, i, divisores;
 	divisores=0;
 	printf("Introducir el numero: \n");
 	scanf("%d", &numero);
+	
+	//Procedimiento
 	for (i=1;i<=numero;i++)
 	{
 		if(numero%i==0)
@@ -12,6 +22,8 @@ int main(int argc, char *argv[]) {
 			divisores++;
 		}
 	}
+	
+	//Salida
 	if (divisores==2)
 	{
 		printf("El numero %d es primo.", numero);
@@ -23,4 +35,12 @@ int main(int argc, char *argv[]) {
 
 	return 0;
 }
-
+/*
+QA Francisco Jesus Mac Cetzal 01/02/2018 07:57 pm
+Entradas./.Salidas./.Resultado
+...997.../.Primo.../..Pimo...OK
+...0...../No Primo./No Primo.OK
+...-3..../No Primo./No Primo.OK
+...853.../.Primo.../..Primo..OK
+..104729./.Primo.../..Primo..OK
+*/
--- Unidad 2-Estructuras de Control/7-sumaPorcentaje.c
+++ Unidad 2-Estructuras de Control/7-sumaPorcentaje.c
@@ -1,12 +1,20 @@
-#include <stdio.h>
-#include <math.h>
+/*Autor: Víctor Ortiz García (30/Enero/18 - 8:00 AM)
+Entradas: numero
+salidas: numerofinal
+Procedimiento General: Captura el número, y según qué tan grande sea se le va sumar ya sea el 5%, 15%, o 20% de sí mismo.
 
+*/
+#include <stdio.h>
+
+//Entrada
 int main(int argc, char *argv[]) {
 	float numero, numerofinal;
 	numero=0; numerofinal=0;
 	printf("Capturar el numero:\n");
 	scanf("%f", &numero);
 	numerofinal=numero;
+	
+	//Procedimiento
 	if (numero>1000)
 	{
 		numerofinal=numero*1.05;
@@ -19,7 +27,18 @@ int main(int argc, char *argv[]) {
 			}
 		}
 	}
+	
+	//Salida
 	printf("El numero final es %f", numerofinal);
 	return 0;
 }
-
+/*
+QA Francisco Jesus Mac Cetzal 01/02/2018 06:25 pm
+Entradas./.Salidas./.Resultado
+..4000.../..4600.../..4600...OK
+..3000.../..3150.../..3150...OK
+...-1..../..Error../....-1...XX (el ejercicio pide que solo se consideren todos los positivos)
+....0..../....0..../....0....OK
+..30000../..36000../..36000..OK
+no incluyo bloque de comentarios
+*/
