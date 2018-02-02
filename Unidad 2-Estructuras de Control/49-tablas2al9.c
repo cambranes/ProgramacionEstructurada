dcf7cae136aa51c3b0498c91d01386a8b2baf210
Flowah155 <31418320+Flowah155@users.noreply.github.com>
2018-02-02 10:34:26 -0600

    Update 49-tablas2al9.c

--- Unidad 2-Estructuras de Control/49-tablas2al9.c
+++ Unidad 2-Estructuras de Control/49-tablas2al9.c
@@ -6,6 +6,21 @@ factorDos (multiplicador)
 Procedimiento general: Se realizan todas las multiplicaciones por medio de dos
 whiles que van multiplicando ambos factores para formar la tabla y se imprimen
 ambos factores junto con el producto.
+
+QA Francisco Jesus Mac Cetzal 01/02/2018 08:35 pm
+Entradas....../.Salidas.................../.Resultado
+"Ejemplo de la tabla del 3"
+3 x 1 = 3 	ok
+3 x 2 = 6 	ok
+3 x 3 = 9 	ok
+3 x 4 = 12 	ok
+3 x 5 = 15	ok
+3 x 6 = 18	ok
+3 x 7 = 21	ok
+3 x 8 = 24	ok
+3 x 9 = 27	ok
+3 x 10 = 30	ok
+notas.- solo como sugerencia traten de separar cada tabla, aparte de eso no hay problemas con el codigo
 */
 
 #include <stdio.h>
@@ -15,7 +30,8 @@ int main(int argc, char *argv[]) {
 	int factorUno=2;
 	int factorDos=1;
 	int producto;
-	/*Procedimiento*/	printf("\nTablas de multiplicar");
+	/*Procedimiento*/
+	printf("\nTablas de multiplicar");
 	while (factorUno<10)
 	{
 		while (factorDos<=10)
