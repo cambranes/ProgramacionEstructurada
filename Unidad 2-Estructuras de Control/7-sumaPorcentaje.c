2f798e49a4027bced575a497c4ad9843301f2246
Flowah155 <31418320+Flowah155@users.noreply.github.com>
2018-02-02 10:35:14 -0600

    Update 7-sumaPorcentaje.c

--- Unidad 2-Estructuras de Control/7-sumaPorcentaje.c
+++ Unidad 2-Estructuras de Control/7-sumaPorcentaje.c
@@ -1,8 +1,16 @@
-/*Autor: Víctor Ortiz García (30/Enero/18 - 8:00 AM)
+/*Autor: VÃ­ctor Ortiz GarcÃ­a (30/Enero/18 - 8:00 AM)
 Entradas: numero
 salidas: numerofinal
-Procedimiento General: Captura el número, y según qué tan grande sea se le va sumar ya sea el 5%, 15%, o 20% de sí mismo.
+Procedimiento General: Captura el nÃºmero, y segÃºn quÃ© tan grande sea se le va sumar ya sea el 5%, 15%, o 20% de sÃ­ mismo.
 
+QA Francisco Jesus Mac Cetzal 01/02/2018 06:25 pm
+Entradas./.Salidas./.Resultado
+..4000.../..4600.../..4600...OK
+..3000.../..3150.../..3150...OK
+...-1..../..Error../....-1...XX (el ejercicio pide que solo se consideren todos los positivos)
+....0..../....0..../....0....OK
+..30000../..36000../..36000..OK
+no incluyo bloque de comentarios
 */
 #include <stdio.h>
 
@@ -32,13 +40,4 @@ int main(int argc, char *argv[]) {
 	printf("El numero final es %f", numerofinal);
 	return 0;
 }
-/*
-QA Francisco Jesus Mac Cetzal 01/02/2018 06:25 pm
-Entradas./.Salidas./.Resultado
-..4000.../..4600.../..4600...OK
-..3000.../..3150.../..3150...OK
-...-1..../..Error../....-1...XX (el ejercicio pide que solo se consideren todos los positivos)
-....0..../....0..../....0....OK
-..30000../..36000../..36000..OK
-no incluyo bloque de comentarios
-*/
+
