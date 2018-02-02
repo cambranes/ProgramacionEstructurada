ccc4bc0d9290ef13e64da9db1306cc1ca7c1f0ff
kristianzoom3 <35499353+kristianzoom3@users.noreply.github.com>
2018-02-01 23:42:36 -0600

    Update #30-Calculo de MCD.c

--- Unidad 2-Estructuras de Control/#30-Calculo de MCD.c
+++ Unidad 2-Estructuras de Control/#30-Calculo de MCD.c
@@ -1,11 +1,7 @@
-30./*Dados 2 numeros enteros Determinar su MCD
-Autor:Kristian Pantoja
-Entradas: #Enteros
-Salida: #Reales*/
+
 #include<stdio.h>
 
 int main() {
-    /*Entradas*/
 	int a;
 	int b;
 	int i;
@@ -15,14 +11,10 @@ int main() {
 	aux=57000;
 	min=57000;
 	ban=1;
-	
-	/*Procedimiento*/
 	printf("Escribir el primer numero a Usar   ");
 	scanf("%i",&a);
 	printf("Escribir el segundo numero a Usar   ");
 	scanf("%i",&b);
-	
-	/*Salidas*/
 	for(i=a;i>1;i--){
 		if(a%i==0){
 			aux=a/i;
