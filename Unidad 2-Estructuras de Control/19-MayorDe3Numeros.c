46b07d088d3f10479488383dfbc066ee1f0e6b19
Elena_Medina <elenamedina155@gmail.com>
2018-02-01 23:12:14 -0600

    Change exercise 19

--- Unidad 2-Estructuras de Control/19-MayorDe3Numeros.c
+++ Unidad 2-Estructuras de Control/19-MayorDe3Numeros.c
@@ -1,12 +1,24 @@
+/* Autor: Elena Soledad Medina Favela
+
+Entradas: numero1, numero2, numero3(Variables que almacenan los valores que van a ser comparados).
+
+Salidas: mayor(Variable que almacena el numero mayor).
+
+Procedimiento General: El usuario ingresa 3 numeros, los cuales seras comparados por medio de 
+una serie de condicionales if para determinar cual de los 3 es el mayor. Al final del proceso 
+se imprime un mensaje indicando cual es el mayor*/
+
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
 	
+	/*Entrada*/
 	float numero1, numero2, numero3, mayor;
 	
 	printf("Ingrese 3 numeros para determinar cual es el mayor\n");
 	scanf("%f %f %f", &numero1, &numero2, &numero3);
 	
+	/*Procedimiento*/
 	if (numero1 > numero2){
 		if (numero1 > numero3){
 			mayor = numero1;
@@ -29,8 +41,17 @@ int main(int argc, char *argv[]) {
 				mayor = numero2;
 			}
 		}
+		else{
+			if(numero2 > numero3){
+				mayor = numero2;
+			}
+			else{
+				mayor = numero3;
+			}
+		}
 	}
 	
+	/*Salida*/
 	printf("El numero mayor es: %f", mayor);
 	return 0;
 }
