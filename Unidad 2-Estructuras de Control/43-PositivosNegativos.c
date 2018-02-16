3f903f38da59f1f5b2b62c8d6cf0b26293182dfa
Elena_Medina <elenamedina155@gmail.com>
2018-02-16 08:52:02 -0600

    Add exercise 1 U3, Change exercises 19, 43 U2

--- Unidad 2-Estructuras de Control/43-PositivosNegativos.c
+++ Unidad 2-Estructuras de Control/43-PositivosNegativos.c
@@ -1,51 +1,53 @@
-/* Autor: Elena Soledad Medina Favela
-
-Entradas: numero(Variable que almacena momentaneamente los valores de los numeros que se van a ingresar).
-
-Salidas: positivo(Variable que almacena el numero de valores positivos ingresados).
-		 negativo(Variable que almacena el numero de valores negativos ingresados).
+/*Autor: Elena Soledad Medina Favela 13/02/2018 20:26 hrs
 
 Procedimiento General: El usuario ingresa n cantidad de numeros para determinar cuantos de ellos
 son positivos y cuantos son negativos. El programa dejara de leer numeros hasta que el usuario 
-ingrese el nuemro 0.
+ingrese el nuemro 0.*/
 
-QA Francisco Jesus Mac Cetzal 01/02/2018 08:30 pm
-Entradas....../.Salidas.................../.Resultado
-1,3,-2,3,0..../.3 positivos 1 negativo..../no ejecuta lectura
--2,-3,-8,-3,0./.4 Negativos.............../no ejecuta lectura
-8,2,9,3,0...../.4 Positivos.............../no ejecuta lectura
-0............./.no se anoto ningun numero./no ejecuta lectura
--1,0........../.1 Negativo................/no ejecuta lectura
-Notas.- no se puso una condicion de entrada para realizar la lectura de los numeros
-		intentar con un valor distinto de entrada o un ciclo Do While.
-*/
-
-#include <stdio.h>
-
-int main(int argc, char *argv[]) {
+	#include <stdio.h>
 	
-	/*Entrada*/
-	int positivo = 0, negativo = 0;
-	float numero = 1;
-	
-	/*Procedimiento*/
-	while(numero != 0){
-		
-		printf("Ingrese un numero: ");
-		scanf("%f", &numero);
-		
-		if(numero > 0){
-			positivo = positivo + 1;
-		}
-		else 
-			if (numero < 0){
-				negativo = negativo + 1;
-			}
+	void lectura(float *);
+	void bucle(float);
+	void imprimir(int, int);
+
+	int main(int argc, char *argv[]) {
+		float numero=1;
+		bucle(numero);
+		return 0;
 	}
 	
-	/*Salida*/
-	printf("Se ingresaron %d numeros positivos \n", positivo);
-	printf("Se ingresaron %d numeros negativos", negativo);
-	return 0;
-}
-
+	/*@param: valor: apuntador que corresponde a la variable "numero" del main, 
+	donde se alojara un numero con punto flotante. 
+	  @return: La funcion no devuelve ningun valor.*/
+	void lectura (float *valor){
+		scanf("%f", valor);
+	}
+	
+	/*@param: numero: Variable que alojara n cantidad de valores, los cuales seran
+	evaluados para determinar si son positivos o negativos. Se dejaran de almacenar
+	valores cuando el usuario ingres eun "0".
+	  @return: La funcion no devuelve ningun valor.*/
+	void bucle (float numero){
+		int positivo=0, negativo=0;
+		while(numero != 0){
+			lectura(&numero);
+			if(numero > 0){
+				positivo = positivo + 1;
+			}
+			else 
+				if (numero < 0){
+					negativo = negativo + 1;
+				}
+		}
+		imprimir(positivo, negativo);
+	}
+	
+	/*@param: positivo, negativo: Variables que llevan el conteo de cuantos numeros 
+	de los ingresados por el usuario son positivos y cuantos son negativos.
+	  @return: La funcion no devuelve ningun valor.*/
+	void imprimir(int positivo, int negativo){
+		printf("Se ingresaron %d numeros positivos \n", positivo);
+		printf("Se ingresaron %d numeros negativos", negativo);
+	}
+	
+	
