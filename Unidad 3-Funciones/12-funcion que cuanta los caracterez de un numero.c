22147331cd75d2bb09603a321350a111c40771ab
kristianzoom3 <35499353+kristianzoom3@users.noreply.github.com>
2018-02-16 23:09:38 -0600

    Add files via upload

---
+++ Unidad 3-Funciones/12-funcion que cuanta los caracterez de un numero.c
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funcion_que_intercambia_numeros_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Entradas
+            int N;
+            Console.WriteLine("Ingresar numero cualesquiera ");
+            N = int.Parse(Console.ReadLine());
+            int M;
+            Console.WriteLine("Ingresar longitud deseada ");
+            M = int.Parse(Console.ReadLine());
+            int Num1 = N;
+            int Num2 = M;
+            Console.ReadKey();
+        }
+        public static void Main()
+        {
+            string Num1 = Num3;
+            Console.WriteLine(Num1.Num2);   
+        }
+    }
+}
+}
