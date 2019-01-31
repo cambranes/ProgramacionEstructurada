70080c22617a15ce0d79dca1669a15dbcfb4c564
Edgar Cambranes <edgar.cambranes@gmail.com>
2019-01-31 08:33:58 -0600

    Reseting Repo
    Updateing Unidad 2

---
+++ Template.c
@@ -0,0 +1,17 @@
+/*
+Autor:Edgar Cambranes 26/Enero/17
+Entradas:
+Salidas:
+Procedimiento general:
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	/*Entradas*/
+	
+	/*Procedimiento*/
+	/*Salidas*/
+	return 0;
+}
+
