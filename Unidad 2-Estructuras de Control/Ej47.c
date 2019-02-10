0962d5e14f4530501a1a1fdb4101cd657014f5f4
alextorrer <46660743+alextorrer@users.noreply.github.com>
2019-02-10 16:25:02 -0600

    Update Ej47.c

--- Unidad 2-Estructuras de Control/Ej47.c
+++ Unidad 2-Estructuras de Control/Ej47.c
@@ -5,8 +5,8 @@
 Autor: Alejandro Torre Reyes
 Entradas: nombre, edad, sueldo por hora, horas trabajadas a la semana, impuestos a pagar.
 Salidas: Sueldo neto total.
-Procesos: El cÛdigo calcula el sueldo neto del empleado donde multiplicando el sueldo por hora por las horas trabajadas
-donde si estas ultimas rebasan las 40 horas semanales, se pagar· doble. TambiÈn se toma en consideraciÛn la resta del
+Procesos: El c√≥digo calcula el sueldo neto del empleado donde multiplicando el sueldo por hora por las horas trabajadas
+donde si estas ultimas rebasan las 40 horas semanales, se pagar√° doble. Tambi√©n se toma en consideraci√≥n la resta del
 porcentaje de impuestos al sueldo neto.
 */
 
@@ -33,8 +33,8 @@ int main()
    printf("Porcentaje de impuestos: ");
    scanf("%d", &impuestos);
 
-   if(horas > 40){ //condicional en caso de que el empleado haya trabajado m·s de 40 horas.
-    horas_extra = 40 - horas;
+   if(horas > 40){ //condicional en caso de que el empleado haya trabajado m√°s de 40 horas.
+    horas_extra = horas - 40;
     horas_tot = ((sueldo_hora)*(horas))+((sueldo_hora)*(horas_extra)); //En caso verdadero; se paga doble.
    }
    else{
