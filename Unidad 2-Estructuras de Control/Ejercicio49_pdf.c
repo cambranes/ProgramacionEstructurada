dd404dc8867d83c772bc43830bdc33666618c083
Rodrigo Hernandez <rodrigoh.gongora@gmail.com>
2019-02-10 21:26:19 -0600

    Cambios a los ejercicios en c
    Se les añadió el formato solicitado por el maestro a los ejercicios de Jorge Pinto

--- Unidad 2-Estructuras de Control/Ejercicio49_pdf.c
+++ Unidad 2-Estructuras de Control/Ejercicio49_pdf.c
@@ -1,8 +1,9 @@
+//49.Escribe el programa para que imprima la tabla de multiplicar completa. Es decir empezando con la tabla del 2, del 3, 4, hasta la del 9. Empieza practicando con la tabla del 2 con una estructura de repetición, nada más y posteriormente utiliza dos ciclos para obtener la tabla completa.
+//Autor: Jorge Pinto
+//Entradas: No se introduce ningun dato
 #include <stdio.h>
-
-
-int main()
-{
+int main(){
+    //Proceso:
   int i, numero = 2, j;
 
   printf("La tabla de multiplicar es : \n\n", numero );
@@ -13,6 +14,6 @@ int main()
       printf("%d * %d = %d \n", i, j, numero);
     }
   }
-
+    //Salidas: Las tablas de multiplicar del 2 al 9
 return 0;
 }
--- Unidad 2-Estructuras de Control/Ejericio14_pdf.c
+++ Unidad 2-Estructuras de Control/Ejericio14_pdf.c
@@ -1,17 +1,18 @@
+//14.Escribir el programa que lea una velocidad e imprima “Muy rápido” si es superior a 100, si no imprima “Detenido” si la velocidad es cero.
+//Autor: Jorge Pinto
+
 #include <stdio.h>
-
-
-
-int main()
-{
+int main(){
+    //Entradas: Un numero entero (velocidad)
   int velocidad;
 printf("Ingresa una velocidad: " );
 scanf("%d", &velocidad );
-
+    //Proceso:
  if (velocidad >= 100) {
    printf("\n Muy rapido");
  } else if (velocidad == 0){
    printf("\n Detenido");
+     //Salidas: la evaluación de los numeros en sus respectivos casos
  }
  return 0;
 }
