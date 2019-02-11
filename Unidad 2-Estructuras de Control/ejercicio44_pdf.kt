e31f55b161dcf171d369736c2d05e784674a1c6e
HectorCosgalla <valcos98@hotmail.com>
2019-02-11 02:17:35 -0600

    QA

--- Unidad 2-Estructuras de Control/ejercicio44_pdf.kt
+++ Unidad 2-Estructuras de Control/ejercicio44_pdf.kt
@@ -12,7 +12,7 @@
 fun main() {
     /* entrada */
     var x = 12; var y = 2
-    
+
     /* procedimiento */
     while (x != y){
         if(x > y){
@@ -21,7 +21,19 @@ fun main() {
             y -= x
         }
     }
-    
+
     /* salida */
-    print(x)   
+    print(x)
 }
+/**
+ * Author: Luis Gerardo Leon Ortega
+ * Entrada: un numero x & y
+ * Salida general: MCD de dos numeros con algoritmo de Euclides
+ * Proceso general:
+ * a. Dividir el mayor de los dos números por el más pequeño
+ * b. A continuación dividir el divisor por el resto
+ * c. Continuar el proceso de dividir el último resto hasta que la división
+ * sea exacta.
+ * d. El último divisor es el MCD
+ * Comentario: Funciona bien
+ */
