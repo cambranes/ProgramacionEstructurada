e241311db95c1be0f1fb5c90e6fcd2a798396033
Luis Leon <leon.luisgerardo775@gmail.com>
2019-02-11 00:16:37 -0600

    - Exercises on Kotlin Lang

---
+++ Unidad 2-Estructuras de Control/ejercicio26_pdf.kt
@@ -0,0 +1,22 @@
+/**
+ * Author: Luis Gerardo Leon Ortega
+ * Entrada: un numero n
+ * Salida general: una suma de los primero n numeros pares
+ * Proceso general:
+ * - La suma de los primeros n numeros pares
+ */
+fun main() {
+    /* entrada */
+    val n = 4 
+    var suma = 0
+ 
+    /* procedimiento */
+    for(i in 2..n){
+        if(i%2 == 0){
+            suma = suma + i;
+        }
+    }
+    
+    /* salida */
+    print(suma)
+}
