568d474d31a9b858ba612c6c01c27b5593637668
emmanuel1993 <emmanuel_azcorra1993@hotmail.com>
2019-02-10 19:26:47 -0600

    Add files via upload

---
+++ Unidad 2-Estructuras de Control/Ej42.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char const *argv[])
+{
+	double n;
+	for (n=1;n<=100; n++){
+	
+	double r = sqrt(n);
+	printf("Raiz cuadrada de %lf es %lf\n",n,r);
+	}
+	return 0;
+}
