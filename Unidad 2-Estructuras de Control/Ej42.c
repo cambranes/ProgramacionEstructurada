f3b2d1970c3701ee7d611ee38809f5ef2b91de6b
emmanuel1993 <emmanuel_azcorra1993@hotmail.com>
2019-02-10 19:45:04 -0600

    Update Ej42.c

--- Unidad 2-Estructuras de Control/Ej42.c
+++ Unidad 2-Estructuras de Control/Ej42.c
@@ -1,3 +1,16 @@
+
+/*
+ 42. raiz cuadrada de los primeros 100 numeros naturales
+Proceso: Escribe el programa que calcule las raíces cuadradas de los cien primeros
+números naturales. Como resultado se desean dos columnas de valores: En
+la primera columna se escribirán los números naturales y en la segunda sus
+raíces correspondientes
+
+Autor: Emmanuel Azcorra Balam
+
+Entradas: 
+*/
+
 #include <stdio.h>
 #include <math.h>
 
@@ -11,3 +24,21 @@ int main(int argc, char const *argv[])
 	}
 	return 0;
 }
+
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char const *argv[])
+{
+	double n;
+	for (n=1;n<=100; n++){
+	
+	double r = sqrt(n);
+	printf("Raiz cuadrada de %lf es %lf\n",n,r);
+	}
+	return 0;
+}
+
+/*
+Salida:raiz cuadrada de los primeros 100 numeros naturales
+*/
