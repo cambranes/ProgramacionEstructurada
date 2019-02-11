e57743ec311c1b6bf489060956e52a26b5954090
HectorCosgalla <valcos98@hotmail.com>
2019-02-11 01:01:08 -0600

    Actualizacion del ejercicio 29

--- Unidad 2-Estructuras de Control/Equipo5/EQ5.Ejercicio29.Hector.rb
+++ Unidad 2-Estructuras de Control/Equipo5/EQ5.Ejercicio29.Hector.rb
@@ -1,7 +1,15 @@
 #!/usr/bin/env ruby
 
 class Ejercicio29
-  Divisor = 2
+  divisor = 2
   puts 'Dame un Numero: '
-  Numero = gets
+  numero = gets.to_i
+  while numero >= divisor
+    while numero % divisor == 0
+      print divisor.to_s + ' x '
+      numero = numero / divisor
+    end
+    divisor = divisor + 1
+  end
+  print numero.to_s
 end
