bea44b5dfe7b9cc5d0157fe9e3697b8cc20608d8
HectorCosgalla <valcos98@hotmail.com>
2019-02-17 14:00:26 -0600

    Update Ejercicio26.js

--- Unidad 3-Funciones/Ejercicio26.js
+++ Unidad 3-Funciones/Ejercicio26.js
@@ -1,3 +1,9 @@
+/*
+Autor: Hector Abraham Valdivieso Cosgalla
+Entrada: Un numero n
+Salida: La suma de los primeros n pares
+Procedimiento: Se lee un numero n y se suman sus primeros n pares
+*/
 var n = prompt("ingresa el numero limite de sumas:");
 SumaNPares(n);
 
