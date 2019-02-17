49b308320a4f52e71b1186a35114aec597e13817
HectorCosgalla <valcos98@hotmail.com>
2019-02-17 15:50:58 -0600

    Actualizacion de Ejercicio34.c y Ejercicio34.js

--- Unidad 3-Funciones/Ejercicio34.js
+++ Unidad 3-Funciones/Ejercicio34.js
@@ -1,3 +1,17 @@
+/*
+Autor: Hector Abraham Valdivieso Cosgalla
+Entradas: Un numero n
+Salidas:
+1
+1 2
+1 2 3
+1 2 3 ... n
+Procedimiento: Lee el numero n y empieza un ciclo for para las lineas y dentro
+deel lleva otro para contar los numeros, si el numero de linea y el numero es el
+mismo se imprime el numero de linea y se brinca a la siguiente linea y se repite
+hasta llegar a n.
+*/
+
 var n = prompt("Ingrese la cantidad de lineas a imprimir: ");
 Nlineas(n);
 
