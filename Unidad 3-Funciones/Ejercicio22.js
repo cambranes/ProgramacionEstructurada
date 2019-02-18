9270c657c16bd052aaa8343e3c4e89a76a432938
fadedphoenix7 <43895609+fadedphoenix7@users.noreply.github.com>
2019-02-18 17:07:13 -0600

    Create Ejercicio22.js

---
+++ Unidad 3-Funciones/Ejercicio22.js
@@ -0,0 +1,40 @@
+/*
+Autor: Jose Eduardo Mendez Verdejo
+Problema:.Determina el valor de la secuencia
+1! + 2! + .. +n!
+Entrada: un número entero
+Salida: la suma de factoriales desde 1 hasta el número seleccionado
+Proceso:Introducir un numero para ir sacando el total del factorial de mayor a menor sumando los resultados en una variable he imprimir el resultado.
+*/
+
+var numero = entrada();
+var i = 1;
+var suma = 0;
+
+while(i <= numero){
+	suma = suma + proceso(i);
+    i = i + 1;
+}
+salida(suma);
+
+
+function entrada(){
+	var numero = parseInt(prompt("Ingrese un numero: "));
+  while(numero < 1){
+    var numero = parseInt(prompt("Ingrese un numero: "));
+  }
+	return numero;
+}
+
+function proceso(i){
+	var factorial = 1, j = 1;
+	for(j; j <= i; j++){
+		factorial = factorial * j;
+	}
+	return factorial;
+}
+
+function salida(numero){
+	alert("La suma de los factoriales es:" + numero);
+}
+	
