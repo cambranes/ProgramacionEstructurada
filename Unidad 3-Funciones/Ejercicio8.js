f2f6a4f9d0e33d2e12cdb1afc08d86cf789022bb
JimmyOjeda <46965072+JimmyOjeda@users.noreply.github.com>
2019-02-18 19:05:13 -0600

    Update Ejercicio8.js

---
+++ Unidad 3-Funciones/Ejercicio8.js
@@ -0,0 +1,10 @@
+/*
+Autor QA: Jimmy Nathan Ojeda Arana
+Entradas: Documento Vacío
+Salidas: Documento Vacío
+Proceso: Documento Vacío
+Modularización: Documento Vacío
+ Función lectura: Documento Vacío
+ Función ascendente: Documento Vacío
+ Función impresion: Documento Vacío
+*/
