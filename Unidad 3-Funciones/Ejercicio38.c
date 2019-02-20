41fdf054bb15beab14033f5a444727098e235e42
fadedphoenix7 <43895609+fadedphoenix7@users.noreply.github.com>
2019-02-19 19:33:47 -0600

    Update and rename ejercicio38.c to Ejercicio38.c

--- Unidad 3-Funciones/Ejercicio38.c
+++ Unidad 3-Funciones/Ejercicio38.c
@@ -7,36 +7,36 @@ Salidas: aquel numero talque la suma de de los cubos de cada digito sea igual al
 Procedimiento: Mostrara aquellos numeros talque la suma de los cubos de los digitos que componen al numero, sean igual al mismo numero
 */
 
-int proceso(int);
+int proceso(int,int,int,int);
 void salida(int);
 
 int main() {
 	int num = 100;
-
+    int x=1, y=0, z=0;
 	while (num < 1000)
 	{
 
-		num = proceso(num);
+		num = proceso(num,x,y,z);
 		num++;
+		z++;
 
 	}
 	return 0;
 }
 
-int proceso(int numero)
+int proceso(int numero, int x, int y, int z)
 {
-	int x,y,z;
 	int s1 = 1, s2 = 1, s3 = 1;
 	int suma = 1;
-	for(x = 1; x<=9 ; x++){
-		for(y = 0; y <= 9; y++){
-			for(z = 0; z <= 9; z++){
+	while(x <= 9){
+		while(y <= 9){
+			while(z <= 9){
 
 				//calculando los cubos de cada digito
 				s1 = x*x*x;
 				s2 = y*y*y;
 				s3 = z*z*z;
-
+                
 				//la suma de los cubos de los digitos
 				suma = s1+s2+s3;
 
@@ -44,15 +44,20 @@ int proceso(int numero)
 					salida(numero);
 				}
 				numero++;
+				z++;
 			}
+			z=0;
+			y++;
 		}
+		y=0;
+		x++;
 	}
+	return numero;
 }
 
 void salida(int num){
 	printf("El numero %i cumple con la condicion \n",num);
 }
-
 /*
 QA: Raul Rivadeneyra
 
