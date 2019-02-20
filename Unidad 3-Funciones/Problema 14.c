e541825e4a7f2a29da560a4e6944c512ddc67a25
Dani2507 <46658875+Dani2507@users.noreply.github.com>
2019-02-20 00:17:34 -0600

    Update Problema 14.c

--- Unidad 3-Funciones/Problema 14.c
+++ Unidad 3-Funciones/Problema 14.c
@@ -1,7 +1,16 @@
+*
+Autor: Daniel Eduardo Gutiérrez Delfín
+Problema:Escribir el programa que lea una velocidad e imprima “Muy rápido” si es
+superior a 100, si no imprima “Detenido” si la velocidad es cero
+Entrada: La velocidad (Velocidad)
+Salida: Muy rapido o Detenido dependiendo la velocidad
+Proceso: Se introduce la velocidad, vemos si es mayor a 100 o igual a 0, dependiendo de eso la variable "resultado" tomara un valor
+	     para que mediante otro if se proyecte el resultado
+*/
 #include <stdio.h>
 
 void valor(int velocidad);
-
+//entrada//
 int main() {
 	int velocidad;
 	printf("Ingrese la velocidad \n");
@@ -9,7 +18,7 @@ int main() {
 	valor(velocidad);
 	return 0;
 }
-
+//Proceso & Salida//
 void valor(int velocidad) {
 	if (velocidad > 100) {
 		printf("Muy rapido!\n");
@@ -17,6 +26,7 @@ void valor(int velocidad) {
 		printf("Detenido\n");
 	}
 }
+
 
 /*
 	QA Reviso: Carlos Chan
