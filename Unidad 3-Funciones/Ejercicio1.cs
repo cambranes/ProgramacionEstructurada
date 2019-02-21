5e8a2b8d401e38ff6b03e37d875a0b24b0cade26
JimmyOjeda <46965072+JimmyOjeda@users.noreply.github.com>
2019-02-21 15:27:40 -0600

    Corrección JimmyQA

--- Unidad 3-Funciones/Ejercicio1.cs
+++ Unidad 3-Funciones/Ejercicio1.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 /*
 Autor: Jimmy Nathan Ojeda Arana
-Entradas: Un numero N
+Entradas: Un numero entero N
 Salidas: Determinar si es par o impar
 Proceso: Dado un numero entero n determina si es par o impar.
 */
@@ -16,14 +16,15 @@ namespace Ejercicio1
     {
         static int Read() //Metodo para leer las entradas
         {
-            int leer;
-            Console.WriteLine("Ingrese n para comprobar su paridad");
+            int numeroN;
+            Console.WriteLine("Ingrese un numero entero para comprobar su paridad");
             numeroN= Convert.ToInt32(Console.ReadLine());
             return numeroN;
         }
 
-        static int Paridad(int paridad) //Metodo para comprobar paridad
+        static int Paridad(int numeroN) //Metodo para comprobar paridad
         {
+            int paridad;
             if (numeroN % 2 ==0){
                 paridad=1;
             }else {
@@ -34,10 +35,8 @@ namespace Ejercicio1
 
         static void Main(string[] args) //Salidas
         {
-            int n;
-            int res;
-            N = Read();
-            par = Paridad(numeroN);
+            int numeroN = Read();
+            int par=Paridad(numeroN);
             if (par==1){
                 Console.WriteLine("El numero {0} es par", numeroN);
             }else {
@@ -47,6 +46,7 @@ namespace Ejercicio1
         }
     }
 }
+
 /* QA: Jorge Chí
 error al compilar:
  
