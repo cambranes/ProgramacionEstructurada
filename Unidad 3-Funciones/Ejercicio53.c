5ad78ae27a554236db0a9f32ebbc44b067d56725
amc <46652567+amaurymc@users.noreply.github.com>
2019-02-21 14:25:51 -0600

    Correcciones de QA Amaury

--- Unidad 3-Funciones/Ejercicio53.c
+++ Unidad 3-Funciones/Ejercicio53.c
@@ -1,7 +1,7 @@
 /*
 Autor: AMAURY MORALES CERECEDO
 Entrada: Cualquier numero, puede ser flotante y negativo.
-Salida: (e^-x)(x).
+Salida: (-e^-x)(x).
 
 Procedimiento general: Utilizando X se hace una serie que toma el valor anterior y lo eleva a -X, despues lo divide por la posicion.
 Tomando en cuenta de que X! = (X-1)!*X , obviamente. Al final el resultado se multiplica otra vez por X.
@@ -10,11 +10,7 @@ Notas: Para utilizar numeros fraccionarios, utilizar su equivalente flotante. Pu
 a infinito.
 
 Al decir ''Leer muchos valores de n'' ayude al usuario a que el programa lea de 1 hasta n infinito, porque asi lo muestra la formula.
-Esto se utiliza para aproximar el valor de nuestra sumatoria hasta (e^-x)(x).
-
-Segun mi entendimiento, es necesario utilizar variables globales si quiero utilizar funciones que sobreescriban valores.
-Es mejor asi en lugar de tener codigo espaghetti que pasa multiples parametros que solo se utilizan una vez debido a que son variables
-locales. El main queda mas limpio, tambien.
+Esto se utiliza para aproximar el valor de nuestra sumatoria hasta (-e^-x)(x).
 
 */
 
@@ -26,45 +22,44 @@ Y = 1 + X + 2X^2/2! + 3X^3/3! - 4X^4/4! + ... + (-1)^n nX^n/n!
 
 #include <stdio.h>
 
-//DECLARACIONES GLOBALES//
-float elevacion = 0; //El numero por el cual e se encuentra elevado.
-int iteracion = 30; //Utilizado para limitar el ciclo.
-float usoTemporal = 1; //Uso temporal.
-float respuesta = 1; //Respuesta temporal.
-float respuestaFinal = 1; //Respuesta final.
-int contador = 0; //Para el ciclo.
-
-void Entrada(),Proceso(float x),Salida();
+void Entrada(float *x),Proceso(float x, float *res),Salida(float res);
 
 int main()
 {
- Entrada();
- Proceso(elevacion);
- Salida();
+ float elevacion = 0; //El numero por el cual e se encuentra elevado.
+ float respuestaFinal = 1; //Respuesta final.
+ Entrada(&elevacion);
+ Proceso(elevacion, &respuestaFinal);
+ Salida(respuestaFinal);
 }
 
-void Entrada()
+void Entrada(float *x)
 {
  printf("Por favor, introduzca x: ");
- scanf("%f", &elevacion);
+ scanf("%f", x);
 }
 
-void Proceso(float x)
+void Proceso(float x, float *res)
 {
+ int iteracion = 30; //Utilizado para limitar el ciclo.
+ float usoTemporal = 1; //Uso temporal.
+ float respuesta = 1; //Respuesta temporal.
+ int contador = 0; //Para el ciclo.
  for (contador = 1; contador<=iteracion; contador++)
  {
-  usoTemporal = (usoTemporal*-x)/contador;
+  usoTemporal = (usoTemporal*x)/contador;
   respuesta = respuesta + usoTemporal;
  }
- respuestaFinal = respuesta*(x);
+ *res = -((x)/respuesta);
 }
 
-void Salida()
+void Salida(float res)
 {
- printf("%f\n", respuestaFinal);
-
- scanf("%f", &usoTemporal); //GetChar no me funciono aqui, asi que solo reescribo una variable.
+ printf("%f\n", res);
+ int x = 0;
+ scanf("%d", &x); //GetChar no me funciono aqui, asi que solo reescribo la variable que esta justo arriba.
 }
+
 /*
 QA: Luis Gerardo Leon Ortega
 Entrada: Cualquier numero, puede ser flotante y negativo.
@@ -74,4 +69,9 @@ Nota 1: Funciona de forma correcta pero no deberia tener variables globales
 
 Nota 2: Las variables no deben ser globales, lo dijo nuestro emperador Cambrane para
  re escribir una variable existen los apuntadores que explico en las primeras clases :p
+ 
+ _________________
+Correcciones hechas
+-Amaury
+ 
 */
