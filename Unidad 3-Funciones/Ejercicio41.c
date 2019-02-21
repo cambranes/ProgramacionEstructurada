bfb31d4ceeefa5c056e20894eb67be866f195d56
amc <46652567+amaurymc@users.noreply.github.com>
2019-02-21 14:20:31 -0600

    Correcciones de QA Amaury

--- Unidad 3-Funciones/Ejercicio41.c
+++ Unidad 3-Funciones/Ejercicio41.c
@@ -8,10 +8,7 @@ Salida: El numero mas grande y el mas chico de todos los numeros dados por el us
 Procedimiento general: Se van cambiando los tronos correspondientes dependiendo de los numeros que el usuario introduzca.
 Al principio se configuran los 2 tronos como el primer numero dado para evitar problemas.
 
-Notas: No se admiten numeros que no sean enteros. Al principio se inicializan los dos tronos con el primer numero porque al introducir
-todos los numeros como negativos, el trono seguira siendo 0 aunque nunca se haya puesto en la lista de numeros a comparar.
-No estoy seguro de cuantos numeros pueda comparar, pero estableci como maximo 99999999 en caso de que por memoria no se puedan realizar mas.
-
+Notas: No se admiten numeros que no sean enteros.
 */
 
 //PREMISA//
@@ -20,52 +17,48 @@ Utilice una estructura de repetición Para...hasta.*/
 
 #include <stdio.h>
 
-//DECLARACIONES GLOBALES//
-int numero, contador, tope, tronoMin, tronoMax; //El numero introducido, el contador de ciclo, el numero maximo de ciclo y los 2 tronos
-int lista[99999999]; //Lista de todos los numeros introducidos por el usuario.
-
-void Entrada(),Proceso(),Salida();
+void Entrada(int *t),Proceso(int t, int *min, int *max),Salida(int min, int max);
 
 int main()
 {
- Entrada();
- Proceso();
- Salida();
+ int tope, tronoMin, tronoMax; //El numero maximo de ciclo
+ Entrada(&tope);
+ Proceso(tope, &tronoMin, &tronoMax);
+ Salida(tronoMin, tronoMax);
 }
 
 /*Funcion entrada
 Solo toma el numero del total de numeros a comparar
 */
-void Entrada()
+void Entrada(int *t)
 {
  printf("Por favor, introduzca el total de numeros a comparar: ");
- scanf("%d", &tope);
+ scanf("%d", t);
 }
 
 /*Funcion proceso
 Toma todos los numeros introducidos por el usuario y determina el mayor y el menor
 */
-void Proceso()
+void Proceso(int t, int *min, int *max)
 {
- for (contador = 0; contador < tope; contador++)
+ int numero, contador; //El numero introducido y el contador
+ for (contador = 0; contador < t; contador++)
  {
   printf("Por favor, introduzca un numero: ");
   scanf("%d", &numero);
 
-  lista[contador] = numero;
-
   if (contador == 0)
   {
-   tronoMin = numero;
-   tronoMax = numero;
+   *min = numero;
+   *max = numero;
   }
-  if (numero < tronoMin)
+  if (numero < *min)
   {
-   tronoMin = numero;
+   *min = numero;
   }
-  if (numero > tronoMax)
+  if (numero > *max)
   {
-   tronoMax = numero;
+   *max = numero;
   }
  }
 }
@@ -73,13 +66,14 @@ void Proceso()
 /*Funcion salida
 Solo imprime el numero mayor y el menor
 */
-void Salida()
+void Salida(int min, int max)
 {
- printf("Numero mayor: %d\n", tronoMax);
- printf("Numero menor: %d\n", tronoMin);
+ printf("Numero mayor: %d\n", max);
+ printf("Numero menor: %d\n", min);
 
- scanf("%d", &tope); //GetChar no me funciono aqui, asi que solo reescribo una variable.
+ scanf("%d", &min); //GetChar no me funciono aqui, asi que solo reescribo una variable.
 }
+
 /*
 QA: Luis Gerardo Leon Ortega
 Entrada: Cualquier numero entero, ya sea positivo o negativo.
@@ -93,4 +87,9 @@ nota 1: El uso de una lista con limite [999999] es ineficiente, estas consumiend
 memoria de forma inecesaria puedes utilizar una lista dinamica
 
 nota 2: Para re escribir una variable utiliza los apuntadores :v
+
+___________________
+Correcciones hechas
+-Amaury
+
 */
