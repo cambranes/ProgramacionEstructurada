789abf18a524cd6517ccdf197a700d6b6e66e5dd
amc <46652567+amaurymc@users.noreply.github.com>
2019-02-21 14:24:43 -0600

    Correcciones de QA Amaury

--- Unidad 3-Funciones/Ejercicio49.cs
+++ Unidad 3-Funciones/Ejercicio49.cs
@@ -6,30 +6,20 @@ Salida: Tablas de multiplicar completas del 2 hasta el 9
 Procedimiento general: Se utilizan dos ciclos con un auxiliar para almacenar cadenas. Estas cadenas son cada una de las tablas de
 multiplicar que van desde el 2 hasta el 9 con maximo 10. Al final se imprimen todas las tablas de multiplicar.
 
-Notas: Segun mi entendimiento, es necesario utilizar variables globales si quiero utilizar funciones que sobreescriban valores.
-Es mejor asi en lugar de tener codigo espaghetti que pasa multiples parametros que solo se utilizan una vez debido a que son variables
-locales. El main queda mas limpio, tambien.
+Notas: Habia una funcion salida la cual fue quitada debido a que me entere de que no se puede utilizar arreglos, AUN cuando
+esto incumple la estructura de que solo se pueden imprimir las salidas en la salida y no en el procedimiento.
 
 */
 
 //PREMISA//
 /*Escribe el programa que imprima la tabla de multiplicar completa. Es decir empezando con la tabla del 2 hasta el 9*/
-
-//DECLARACIONES GLOBALES//
-int numeroInicial = 2; //Desde donde empieza nuestras tablas
-int numeroFinal = 9; //Hasta que numero llegan nuestras tablas
-
-int contadorI = 1; //Para el ciclo
-int contadorO = 1; //Para el ciclo
-int constanteA = 1; //Auxiliar
-
-string[] lista = new string[81]; //La unica forma de poder tener la salida en el bloque Salida y no en Proceso.
-
 void main()
 {
+ int numeroInicial = 2; //Desde donde empieza nuestras tablas
+ int numeroFinal = 9; //Hasta que numero llegan nuestras tablas
  Entrada();
  Proceso(numeroInicial, numeroFinal);
- Salida();
+ //Salida();   Aqui antes habia una funcion para imprimir todas las tablas utilizando listas.
 }
 
 /*Funcion entrada
@@ -45,27 +35,17 @@ Escribe las tablas de multiplicar en una lista
 */
 void Proceso(int a, int b)
 {
+ int contadorI = 1; //Para el ciclo
+ int contadorO = 1; //Para el ciclo
+ int constanteA = 1; //Auxiliar
  for (contadorO = a; contadorO <= b; contadorO++)
  {
   contadorI = 1;
   for (contadorI = 1; contadorI <= 10; contadorI++)
   {
-   lista[constanteA] = ""+contadorO+"x"+contadorI+" = "+contadorO*contadorI;
+   Console.WriteLine(""+contadorO+"x"+contadorI+" = "+contadorO*contadorI);
    constanteA++;
   }
- }
-}
-
-/*Funcion salida
-Solo imprime las tablas de multiplicar almacenadas en una lista
-*/
-void Salida()
-{
- Console.WriteLine("");
- contadorI = 1;
- for (contadorI = 1; contadorI <= 80; contadorI++)
- {
-  Console.WriteLine(lista[contadorI]);
  }
 }
 
@@ -77,4 +57,10 @@ main(); //Para iniciar el programa.
     
     Software usado para el test: https://code.sololearn.com/#cs
     (Recomendado por Alex en entrega anterior para el compilado de C#)
+    
+    __________________________________________________
+    Que raro que no te funcione, porque a mi si :)
+    Usa otro compilador entonces (?)
+    -Amaury
+    
 */
