0876fd44355515ffde7744e7152d336c7d5d4ed7
alextorrer <46660743+alextorrer@users.noreply.github.com>
2019-02-21 14:32:31 -0600

    Correcion AlexQA

--- Unidad 3-Funciones/Ejercicio25.c
+++ Unidad 3-Funciones/Ejercicio25.c
@@ -5,24 +5,34 @@
 Autor: Alejandro Torre Reyes
 Entradas: una letra, minuscula o mayuscula
 Salidas: El codigo ASCII para esa letra
-Proceso: Escribir un programa para mostrar los códigos ASCII de las letras
-mayúsculas y minúsculas.
+Proceso: Escribir un programa para mostrar los cÃ³digos ASCII de las letras
+mayÃºsculas y minÃºsculas.
 */
+int c_ascii();
+char entrada();
 int main()
 {
-    //Entradas
     char letra;
     int cod;
-    printf("Ingresa una letra \n");
-    scanf("%c",&letra);
-
-    //Proceso
-    cod=("%d",letra);
-
-    //Salida
+    
+    letra = entrada();
+    cod = c_ascii(letra);
+    
     printf("El codigo ASCII para %c es %d",letra,cod);
     return 0;
 }
+char entrada(){
+    char letter;
+    printf("Ingresa una letra \n");
+    scanf("%c",&letter);
+    return letter;
+}
+
+int c_ascii(int x){
+    int y = ("%d",x);
+    return y;
+}
+
 //QA realizado por Rodrigo Hernandez Gongora
 //Entradas probadas: r, a, T, F
 //Salidas: 114, 97, 84, 70
