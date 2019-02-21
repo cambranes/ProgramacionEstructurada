97471df0ee45a97936bdd4bc3b9703ff8b520139
amc <46652567+amaurymc@users.noreply.github.com>
2019-02-21 14:26:50 -0600

    Correcciones de QA Amaury

--- Unidad 3-Funciones/Ejercicio53.cs
+++ Unidad 3-Funciones/Ejercicio53.cs
@@ -1,7 +1,7 @@
 /*
 Autor: AMAURY MORALES CERECEDO
 Entrada: Cualquier numero, puede ser negativo. No introducir numeros decimales.
-Salida: (e^-x)(x).
+Salida: (-e^-x)(x).
 
 Procedimiento general: Utilizando X se hace una serie que toma el valor anterior y lo eleva a -X, despues lo divide por la posicion.
 Tomando en cuenta de que X! = (X-1)!*X , obviamente. Al final el resultado se multiplica otra vez por X.
@@ -10,11 +10,7 @@ Notas: Puede que numeros muy grandes la respuesta sea equivalente a infinito.
 Se necesitan procesos mas complicados para pasar el input del usuario a un valor flotante en C#. Por ahora solo lo limito a enteros.
 
 Al decir ''Leer muchos valores de n'' ayude al usuario a que el programa lea de 1 hasta n infinito, porque asi lo muestra la formula.
-Esto se utiliza para aproximar el valor de nuestra sumatoria hasta (e^-x)(x).
-
-Segun mi entendimiento, es necesario utilizar variables globales si quiero utilizar funciones que sobreescriban valores.
-Es mejor asi en lugar de tener codigo espaghetti que pasa multiples parametros que solo se utilizan una vez debido a que son variables
-locales. El main queda mas limpio, tambien.
+Esto se utiliza para aproximar el valor de nuestra sumatoria hasta (-e^-x)(x).
 
 */
 
@@ -24,19 +20,13 @@ Escribe el programa que lea muchos valores de n y que calcule la expresión sigu
 Y = 1 + X + 2X^2/2! + 3X^3/3! - 4X^4/4! + ... + (-1)^n nX^n/n!
 */
 
-//DECLARACIONES GLOBALES//
-int elevacion = 0; //El numero por el cual e se encuentra elevado.
-int iteracion = 30; //Utilizado para limitar el ciclo.
-float usoTemporal = 1; //Uso temporal.
-float respuesta = 1; //Respuesta temporal.
-float respuestaFinal = 1; //Respuesta final.
-int contador = 0; //Para el ciclo.
-
 void main()
 {
- Entrada();
- Proceso(elevacion);
- Salida();
+ int elevacion = 0; //El numero por el cual e se encuentra elevado.
+ float respuestaFinal = 1; //Respuesta final.
+ Entrada(ref elevacion);
+ Proceso(elevacion, ref respuestaFinal);
+ Salida(ref respuestaFinal);
 }
 
 /*Funcion Input A Numero
@@ -52,31 +42,35 @@ int Input_A_Numero()
 /*Funcion Entrada
 Toma el input del usuario de el valor de X.
 */
-void Entrada()
+void Entrada(ref int x)
 {
  Console.WriteLine("Por favor, introduzca X: ");
- elevacion = Input_A_Numero();
+ x = Input_A_Numero();
 }
 
 /*Funcion Proceso
 Calcula nuestra formula con el valor introducido por el usuario.
 */
-void Proceso(int x)
+void Proceso(int x, ref float res)
 {
+ int iteracion = 30; //Utilizado para limitar el ciclo.
+ float usoTemporal = 1; //Uso temporal.
+ float respuesta = 1; //Respuesta temporal.
+ int contador = 0; //Para el ciclo.
  for (contador = 1; contador<=iteracion; contador++)
  {
-  usoTemporal = (usoTemporal*-x)/contador;
+  usoTemporal = (usoTemporal*x)/contador;
   respuesta = respuesta + usoTemporal;
  }
- respuestaFinal = respuesta*(x);
+ res = -((x)/respuesta);
 }
 
 /*Funcion Salida
 Imprime el resultado final.
 */
-void Salida()
+void Salida(ref float res)
 {
- Console.WriteLine(respuestaFinal);
+ Console.WriteLine(res);
 }
 
 main(); //Para iniciar el programa.
@@ -90,4 +84,8 @@ Nota 1: Funciona de forma correcta pero no deberia tener variables globales
 
 Nota 2: Las variables no deben ser globales, lo dijo nuestro emperador Cambrane para
  re escribir una variable existen los apuntadores que explico en las primeras clases :p
+ 
+ ___________
+ Correcciones hechas
+ -Amaury
 */
