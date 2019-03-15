afc9759dbf9ec6c70c7ed18400d7373548f01f54
Sailest <43819431+Sailest@users.noreply.github.com>
2019-03-15 01:53:15 -0600

    Update Ejercicio 13.c

--- Unidad 4-Arreglos/Ejercicio13.c
+++ Unidad 4-Arreglos/Ejercicio13.c
@@ -6,29 +6,58 @@ sumar en orden cada parte del vector
 Salidas: El vector resultante de la suma de ambos
 */
 #include <stdio.h>
+#define SIZE 256
+
+void asignarVacioArreglos(char cadenaUno[SIZE], char cadenaDos[SIZE], int transUno[SIZE], int transDos[SIZE]);
+void transicionCaracterNumero(char cadenaUno[SIZE], char cadenaDos[SIZE], int transUno[SIZE], int transDos[SIZE]);
+void validacionNumerica(char cadenaUno[SIZE], char cadenaDos[SIZE], int*);
+void sumaDeVectores(char cadenaUno[SIZE], char cadenaDos[SIZE], int transUno[SIZE], int transDos[SIZE], int resulCadena[SIZE], int*);
 
 int main(int argc, char *argv[]) {
 	//entrada
 	int valida = 1;
-	char cadenaUno[256];
-	char cadenaDos[256];
-	int transUno[256];
-	int transDos[256];
-	int resulCadena[256];
+	char cadenaUno[SIZE];
+	char cadenaDos[SIZE];
+	int transUno[SIZE];
+	int transDos[SIZE];
+	int resulCadena[SIZE];
+
+	//proceso
+	//Asignar valores 0 a todos los arreglos
+	asignarVacioArreglos(cadenaUno, cadenaDos, transUno, transDos);
+	
+	//Leer los arreglos
 	scanf(" %s", cadenaUno);
 	scanf(" %s", cadenaDos);
 	
+	//Cambiar de caracter a numero
+	transicionCaracterNumero(cadenaUno, cadenaDos, transUno, transDos);
 	
-	//proceso
-	for (int i = 0; i < 256; i++){
+	//Asegurarse de que el arreglo solo contiene caracteres numericos
+	validacionNumerica(cadenaUno, cadenaDos, &valida);
+	
+	//Imprimir si hay un caracter no numerico
+	if(valida == 0){
+		printf("\n ERROR: un caracter no es numero\n");
+	}
+	//Sumar los vectores 
+	sumaDeVectores(cadenaUno, cadenaDos, transUno, transDos, resulCadena, &valida);
+	return 0;
+}
+
+//Asignar valores 0 a todos los arreglos
+void asignarVacioArreglos(char cadenaUno[SIZE], char cadenaDos[SIZE], int transUno[SIZE], int transDos[SIZE]){
+	for (int i = 0; i < SIZE; i++){
 		cadenaUno[i] = 0;
 		cadenaDos[i] = 0;
 		transUno[i] = 0;
 		transDos[i] = 0;
 	}
-  scanf(" %s", cadenaUno);
-	scanf(" %s", cadenaDos);
-	for (int i = 0; i < 256; i++){
+}
+
+//Cambiar de caracter a numero
+void transicionCaracterNumero(char cadenaUno[SIZE], char cadenaDos[SIZE], int transUno[SIZE], int transDos[SIZE]){
+	for (int i = 0; i < SIZE; i++){
 		if(cadenaUno[i] != 0){
 			transUno[i] = cadenaUno[i] - 48;
 		}
@@ -36,38 +65,38 @@ int main(int argc, char *argv[]) {
 			transDos[i] = cadenaDos[i] - 48;
 		}
 	}
-	for(int i = 0; i < 256; i++){
-		if((cadenaUno[i] <= 48) || (cadenaUno[i] >= 57) || (cadenaUno == 0)){
-			valida = 0;
-			break;
-		}
-	}
-	for(int i = 0; i < 256; i++){
-		if((cadenaDos[i] <= 48) || (cadenaDos[i] >= 57) || (cadenaDos == 0)){
-			valida = 0;
-			break;
-		}
-	}
-	if(valida == 0){
-  
-  //Salida
-		printf("\n ERROR: un caracter no es numero\n");
-	}
-	if(valida == 1){
-		printf("La cadena de caracteres es: {");
-		for (int i = 0; i < 256 ; i++){
+}
+
+//Asegurarse de que el arreglo solo contiene caracteres numericos
+void validacionNumerica(char cadenaUno[SIZE], char cadenaDos[SIZE], int *valida){
+	for(int i = 0; i < SIZE; i++){
+		if(((cadenaUno[i] < 48) || (cadenaUno[i] > 57)) && (cadenaUno[i] != 0)){
+			*valida = 0;
+			i = SIZE;
 			
+		}
+	}
+	for(int i = 0; i < SIZE; i++){
+		if(((cadenaDos[i] < 48) || (cadenaDos[i] > 57)) && (cadenaDos[i] != 0)){
+			*valida = 0;
+			i = SIZE;
+			
+		}
+	}
+}
+
+
+void sumaDeVectores(char cadenaUno[SIZE], char cadenaDos[SIZE], int transUno[SIZE], int transDos[SIZE], int resulCadena[SIZE], int *valida){
+	if( *valida == 1){
+		printf("La cadena de caracteres es: {");
+		for (int i = 0; i < SIZE ; i++){
 			resulCadena[i] = transUno [i] + transDos[i];
 			if((cadenaUno[i] == 0) && (cadenaDos[i] == 0)){
 				break;
 			}
-			
 			printf("%d",resulCadena[i]);
-			if(cadenaDos[i + 1] != 0){
-				printf(", ");
-			}
+			printf(" ");
 		}
 		printf("}");
 	}
-	return 0;
 }
