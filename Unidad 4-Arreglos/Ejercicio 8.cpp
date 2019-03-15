61935f224706559d0fdc31690d24bfff0ffd2aae
JimmyOjeda <46965072+JimmyOjeda@users.noreply.github.com>
2019-03-14 18:35:15 -0600

    QA Corregido

--- Unidad 4-Arreglos/Ejercicio 8.cpp
+++ Unidad 4-Arreglos/Ejercicio 8.cpp
@@ -16,84 +16,67 @@ using namespace std;
 void rellenarVector(int*,int,int);
 void imprimirVector(int*,int,int);
 float funcionMedia(int*,int,int);
+float funcionVarianza(int*,int,float,int);
 void funcionDesviacionEstandar(int*,int,float,int);
-void funcionVarianza(int*,int,float,int);
 
 int main()
 {
-	srand(time(NULL));
-	int n;
-	cout<<"Ingresa la cantidad de numeros del vector"<<endl;
-	cin>>n;
-	const int tamanio=n;
-	int vectorN[tamanio];
-	rellenarVector(vectorN,tamanio,n);
-	cout<<"Los numeros generados en el vector son"<<endl;
-	imprimirVector(vectorN,tamanio,n);
-	funcionMedia(vectorN,tamanio,n);
-	//Se guarda el valor del promedio para poder ser usado en otras funciones
-	float promedio=funcionMedia(vectorN,tamanio,n);
-	cout<<"El promedio es "<<promedio<<endl;
-	funcionDesviacionEstandar(vectorN,tamanio,promedio,n);
-	funcionVarianza(vectorN,tamanio,promedio,n);
-	
-	return 0;
+    srand(time(NULL));
+    int n;
+    float valorVarianza;
+    cout<<"Ingresa la cantidad de numeros del vector"<<endl;
+    cin>>n;
+    const int tamanio=n;
+    int vectorN[tamanio];
+    rellenarVector(vectorN,tamanio,n);
+    cout<<"Los numeros generados en el vector son"<<endl;
+    imprimirVector(vectorN,tamanio,n);
+    funcionMedia(vectorN,tamanio,n);
+    float promedio=funcionMedia(vectorN,tamanio,n);
+    cout<<"\nEl promedio es "<<promedio<<endl;
+    funcionDesviacionEstandar(vectorN,tamanio,promedio,n);
+    valorVarianza=funcionVarianza(vectorN,tamanio,promedio,n);
+    cout<<"La varianza es "<<valorVarianza;
+
+    return 0;
 }
 
 void rellenarVector(int* vectorN,int tamanio,int n){
-	for (int i=0;i<n;i++){
-		//Se generan numeros al azar del 1 al 100
-		vectorN[i]=rand()%101;
-	}
+    for (int i=0;i<n;i++){
+        vectorN[i]=rand()%101;
+    }
 }
 
 void imprimirVector(int* vectorN,int tamanio,int n){
-	for (int i=0;i<n;i++){
-		//Se imprime el vector generado
-		cout<<vectorN[i]<<endl;
-	}
+    for (int i=0;i<n;i++){
+        cout<<vectorN[i]<<"  ";
+    }
 }
 
 float funcionMedia(int* vectorN,int tamanio,int n){
-	float sumaNumeros=0;
-	for (int i=0;i<n;i++){
-		sumaNumeros=sumaNumeros+vectorN[i];
-	}
-	//Se devuelve el promedio
-	return sumaNumeros/n;
+    float sumaNumeros=0;
+    for (int i=0;i<n;i++){
+        sumaNumeros=sumaNumeros+vectorN[i];
+    }
+    return sumaNumeros/n;
+}
+
+float funcionVarianza(int* vectorN,int tamanio,float promedio,int n){
+    float diferenciaAPromedio,diferenciaAPromedioCuadrada;
+    float sumaDeDiferenciasCuadradas=0;
+    float varianza;
+    for (int i=0;i<n;i++){
+        diferenciaAPromedio=promedio-vectorN[i];
+        diferenciaAPromedioCuadrada=diferenciaAPromedio*diferenciaAPromedio;
+        sumaDeDiferenciasCuadradas=sumaDeDiferenciasCuadradas+diferenciaAPromedioCuadrada;
+    }
+    return sumaDeDiferenciasCuadradas/n;
 }
 
 void funcionDesviacionEstandar(int* vectorN,int tamanio,float promedio,int n){
-	int diferenciaAPromedio,diferenciaAPromedioCuadrada;
-	int sumaDeDiferenciasCuadradas=0;
-	float desviacionEstandar;
-	//Se halla la diferencia del promedio con cada numero del vector
-	for (int i=0;i<n;i++){
-		diferenciaAPromedio=promedio-vectorN[i];
-		//Se eleva al cuadrado dicha diferencia
-		diferenciaAPromedioCuadrada=diferenciaAPromedio*diferenciaAPromedio;
-		//Se suman todas las diferencias al cuadrado
-		sumaDeDiferenciasCuadradas=sumaDeDiferenciasCuadradas+diferenciaAPromedioCuadrada;
-	}
-	//Se saca la raiz cuadrada de la division de las diferencias al cuadrado entre el numero n de elementos del vector
-	desviacionEstandar=sqrt(sumaDeDiferenciasCuadradas/n);
-	cout<<"La desviacion estandar es "<<desviacionEstandar<<endl;
-}
-
-void funcionVarianza(int* vectorN,int tamanio,float promedio,int n){
-	int diferenciaAPromedio,diferenciaAPromedioCuadrada;
-	int sumaDeDiferenciasCuadradas=0;
-	float varianza;
-	for (int i=0;i<n;i++){
-		//Se halla la diferencia del promedio con cada numero del vector
-		diferenciaAPromedio=promedio-vectorN[i];
-		//Se eleva al cuadrado dicha diferencia
-		diferenciaAPromedioCuadrada=diferenciaAPromedio*diferenciaAPromedio;
-		//Se suman todas las diferencias al cuadrado
-		sumaDeDiferenciasCuadradas=sumaDeDiferenciasCuadradas+diferenciaAPromedioCuadrada;
-	}
-	//Se divide entre n la cantidad obtenida de la suma
-	cout<<"La varianza es "<<sumaDeDiferenciasCuadradas/n;
+    float desviacionEstandar;
+    desviacionEstandar=sqrt(funcionVarianza(vectorN,tamanio,promedio,n));
+    cout<<"La desviacion estandar es "<<desviacionEstandar<<endl;
 }
 
 /*
@@ -124,5 +107,7 @@ funcion funcionvarianza(Pequeños detalles): La calcula casi de manera precisa
 
 Comentarios: declaraas una constante que puede remplazar todas las n, hasta en las funciones
 Por lo demas, pequeños arreglos que se mencionan arriba, pero funciona corretamente.  :)
+
+Jimmy Ojeda: Correcciones realizadas.
 
 */	
