401a0b69d4c34a0ba1e959802fe3f84ad11a0bd7
RaulRivadeneyra <rivadeneyraburgos.r@gmail.com>
2019-03-15 07:49:40 -0600

    Update ejercicio2.c

--- Unidad 4-Arreglos/ejercicio2.c
+++ Unidad 4-Arreglos/ejercicio2.c
@@ -6,18 +6,27 @@
 */
 #include "stdio.h"
 
+
+
 #define m 4
 #define n 4
+
+void process(int[m][n], int[m][n], int[m][n])
+
 int main() {
   int A[m][n];
   int B[m][n];
   int S[m][n];
+  process(A,B,S);
+  return 0;
+}
+
+void process(int A[m][n], int B[m][n], int S[m][n]){
   for (int i = 0; i < m; i++) {
-    for (int j = 0; j < n; j++) {
-      S[i][j] = A[i][j] + B[i][j];
+    for  for (int j = 0; j < n; j++) {
+        S[i][j] = A[i][j] + B[i][j];
     }
   }
-  return 0;
 }
 /*QA: Jorge Chí
 Entradas:1 y 2 para A[m][n] y B[m][n], Salidas: 3,3,3,3,3,3,3,3...,3.
