55584a601fc81625250a45da0bc837380904350e
Ivan Farid Espadas Escalante <farid.espadas@gmail.com>
2020-02-03 15:32:15 -0600

    Documentación ejercicio 38
    Este ejercicio no requierio modificaciones, solo documentacion.

--- Unidad 2-Estructuras de Control/Equipo2_CodePain/Ejercicio38.py
+++ Unidad 2-Estructuras de Control/Equipo2_CodePain/Ejercicio38.py
@@ -1,18 +1,23 @@
-num=100
-numTemp=0
-aDig=0
-bDig=0
-cDig=0
-total=0
 
-while num<1000:
-    numTemp=num
-    cDig=numTemp%10
-    numTemp=(numTemp-numTemp%10)/10
-    bDig=numTemp%10
-    numTemp=(numTemp-numTemp%10)/10
-    aDig=numTemp
-    total=aDig**3+bDig**3+cDig**3
-    if total==num:
-        print(str(num)+ " ")
+#Declaración de variables
+num = 100 #Inicio del número donde empieza la cuenta de 3 digitos, para comparar al final
+numTemp = 0 #Aquí se almacena temporalmente el número para ir guardando los digitos
+aDig = 0 #Aquí se guarda el primer digito
+bDig = 0 #Aquí se guarda el segundo digito
+cDig = 0 #Aquí se guarda el tercer digito
+total = 0 #Aquí se guarda el total de elevar a la potencia 3 los digitos del número
+
+
+#Condicional para comprobar cada uno desde el 100 al 999
+#Salida: Los números que cumplan que sus digitos elevados a la tercera potencia sean igual a ese mismo número
+while num < 1000:
+    numTemp = num
+    cDig = numTemp%10
+    numTemp = (numTemp-numTemp%10)/10
+    bDig = numTemp%10
+    numTemp = (numTemp-numTemp%10)/10
+    aDig = numTemp
+    total = aDig**3 + bDig**3 + cDig**3
+    if total == num:
+        print(str(num) + " ")
     num=num+1
