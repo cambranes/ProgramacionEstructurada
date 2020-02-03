a85ba3810d66636e9fc6e288e25282bc0bca9c2d
Jonathan Gómez <joni_1200@hotmail.com>
2020-02-03 14:39:16 -0600

    Update Ejercicios
    Documentación de ejercicios 8, 20, 32, 44

--- Unidad 2-Estructuras de Control/Equipo2_CodePain/Ejercicio20.py
+++ Unidad 2-Estructuras de Control/Equipo2_CodePain/Ejercicio20.py
@@ -1,9 +1,17 @@
+#Autores: Programa realizado por el Equipo2_CodePain
+#Programa que determina el valor de una secuencia 1 a N
+
+
 nNums=-1
 total=0
 
+#Entrada: Límite de la secuencia (nNums)
+#Proceso: Se obtiene el valor de la secuencia al sumar cada número empezando por 1
 while nNums<0:
     nNums=int(input())
 
 total=(nNums*(nNums+1))/2
 
+
+#Salida: La suma total de la secuencia de números
 print(total)
