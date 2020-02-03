3bfae3b53176ef6bf4a48db3b2e41e7ac62d6351
Ivan Farid Espadas Escalante <farid.espadas@gmail.com>
2020-02-03 13:48:43 -0600

    Update Ejercicio2.py
    Documentación de ejercicio 2

--- Unidad 2-Estructuras de Control/Equipo2_CodePain/Ejercicio2.py
+++ Unidad 2-Estructuras de Control/Equipo2_CodePain/Ejercicio2.py
@@ -1,26 +1,27 @@
-print("Este programa recibe dos números y nos dice cual es mayor, menor o si ambos son iguales." + "\n")
+# Este programa recibe dos números y nos dice cual es mayor, menor o si ambos son iguales.
 
 #Inicio declaración de variables
 
 aNum = 0 #Primer número a leer
 bNum = 0 #Segundo número a leer 
+iguales =  "Los números son iguales"
 
 #Fin declaración de variables
 
 #Inicio de lectura de números 
 
-aNum = float(input("Ingrese el primer número: " + "\n"))
-bNum = float(input("Ingrese el segundo número: " + "\n"))
+aNum = float(input()) #Leer primer número 
+bNum = float(input()) #Leer segundo número
 
 #Fin lectura de números 
 
 #Inicio condicionales para asignación e impresión de quien es mayor, menor o si son iguales 
 
 if aNum > bNum:
-    print("El número mayor es " + str(aNum) + " y el número menor es " + str(bNum))
+    print(str(aNum) + " > " + str(bNum)) #La primera salida es el número más grande y la segunda el más chico
 elif (aNum < bNum):
-    print("El número mayor es " + str(bNum) + " y el número menor es " + str(aNum))
+    print(str(bNum) + " > " +  str(aNum)) #La primera salida es el número más grande y la segunda el más chico
 else: 
-    print("Ambos números son iguales")
+    print(iguales)
 
 #Fin condicionales para asignación e impresión de quien es mayor, menor o si son iguales
