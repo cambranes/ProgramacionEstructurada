a2e018df221fb46997c663a5b834e4e0603037ac
Ivan Farid Espadas Escalante <farid.espadas@gmail.com>
2020-02-03 14:09:22 -0600

    Documentacion ejercicio 14
    Puse comentarios y especifique secciones.

--- Unidad 2-Estructuras de Control/Equipo2_CodePain/Ejercicio14.py
+++ Unidad 2-Estructuras de Control/Equipo2_CodePain/Ejercicio14.py
@@ -1,22 +1,19 @@
 #Este programa lee una velocidad y determina si es mayor a 100, imprime "muy rapido" y si es cero, imprime "detenido"
 
+
 #Inicio de declaración e inicialización de variables
-
 vel = 0 #Variable que almacenara la velocidad
-
 #Fin de declaración e inicialización de variables
 
-#Inicio lectura de velocidad
 
+#Inicio lectura de velocidad
+#Entrada: Cualquier número real 
 vel = float(input()) #Leer velocidad
 
-#Fin lectura de velocidad
 
-#Inicio para determinar si la velocidad es mayor a 100 o cero
-
+#Condiconal para determinar si es mayor a cien o cero la velocidad
+#Salida: "Muy rápido" o "Detenido" o nada si no es cero o mayor a cien
 if (vel > 100):
     print("Muy rápido")
 elif (vel == 0):
     print("Detenido")
-
-#Fin para determinar si la velocidad es mayor a 100 o cero
