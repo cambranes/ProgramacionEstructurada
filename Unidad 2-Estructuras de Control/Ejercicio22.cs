f3d81bad28fe80ff8644bf0740efb56f4c33872f
CarlosGreene <59940440+CarlosGreene@users.noreply.github.com>
2020-02-04 02:02:04 -0600

    3 ejercicios del ada 2

---
+++ Unidad 2-Estructuras de Control/Ejercicio22.cs
@@ -0,0 +1,45 @@
+﻿//Equipo4
+//Fecha 04/02/2020
+//Programa que hace una suma los factoriales de una serie de n números
+using System;
+
+namespace Ejercicio22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Entrada
+            int entrada = 0, fact = 1, salida = 0;
+            // entrada: número introducido, fact: factorial, salida: La suma de la seria de factoriales
+
+            Console.WriteLine("Introduce el número de serie");
+            int.TryParse(Console.ReadLine(), out entrada); //Se lee el número de entrada
+
+            //Proceso
+            if(entrada == 0) //Se compara si el número introducido es cero
+            {
+                salida = 1; //Si el número es cero, entonces su factorial será 1 por conveniencia
+            }
+            else
+            { //Si no es igual a cero entonces
+              for(int i = 1; i <= entrada; i++) //Se hace un ciclo repetitivo para sumar la serie de factoriales
+                {
+                    fact=1; //Se inicializa la variable factorial 1
+
+                    for(int n = 1; n <= i; n++) //Se hace otro ciclo repetitivo para calcular factoriales
+                    {
+                        fact = fact*n; //Se calcula factorial multiplicando la veriable fact por n del ciclo
+                    }
+
+                    salida = salida + fact; //Se suma los factoriales
+
+                }  
+            }
+
+            //Salida
+            Console.WriteLine("El total de la serie de factoriales es:");
+            Console.WriteLine(salida); //Se imprime la suma de serie de factoriales
+        }
+    }
+}
---
+++ Unidad 2-Estructuras de Control/Ejercicio4.cs
@@ -0,0 +1,37 @@
+﻿//Equipo4
+//Fecha 04/02/2020
+//Programa que redondea un número a la centena próxima
+using System;
+
+namespace Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Entrada
+            int entrada = 0, r = 0, salida = 0;
+            //entrada: número introducido, d: residuo de módulo 100, salida: número redondeado a la centena próximo
+
+            Console.WriteLine("Escribe un número");
+            int.TryParse(Console.ReadLine(), out entrada); //Se lee la entrada
+
+            r = entrada%100; //Se hace módulo 100 de la entrada para obtener el residuo
+
+            //Proceso
+            if(r>50) //Se compara si el residuo es mayor que 50
+            {   //Si es mayor a 50 entonces
+                salida = ((entrada/100)+1)*100; //Se divide la entrada entre 100, se suma 1 y se mutiplica por 100 
+            }                                   //para rendear el número a centena mayor
+            else
+            {   //Si es menor o igual a 20
+                salida = (entrada/100)*100; //Se divide la entrada entre 100 y se divide entre 100
+            }                               //para redondearlo a la centena menor
+
+            //Salida
+            Console.WriteLine("El número {0} redondeado a centenar es:", entrada);
+            Console.WriteLine(salida); //Se imprime el número redondeado 
+
+        }
+    }
+}
