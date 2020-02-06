e12e9f7c5b1f8f86e4505adf5430d9704adb926a
Pablotob <fringe1224@gmail.com>
2020-02-06 12:43:02 -0600

    ejerciciosequi´po6

---
+++ Unidad 2-Estructuras de Control/ejercicio6.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class ejercicio6
+{
+    public static void main(String[] args)
+    {
+        Scanner reader = new Scanner(System.in);                
+        int cateto1, cateto2, hipotenusa;
+        System.out.println("Ingrese el valor del primer cateto: ");
+        cateto1 = reader.nextInt();
+
+        System.out.println("ingrese el valor del segundo catero: ");
+        cateto2 = reader.nextInt();
+
+        System.out.println("Ingrese el valor de la hipotenusa: ");
+        hipotenusa = reader.nextInt();
+
+
+      Number hipotenusareal;
+        hipotenusareal= Math.sqrt(Math.pow(cateto1, 2) + Math.pow(cateto2, 2));
+        
+        Number hipotenus, hipotenusarea;
+        hipotenusarea=hipotenusareal;
+        hipotenus=hipotenusa;
+        
+
+        if ( hipotenus == hipotenusarea) {
+
+            System.out.printf ("Los Numeros Si Cumplen Con El Teorema De Pitagoras");
+    
+    } else {
+    
+        System.out.printf ("Los Numeros No Cumplen Con El Teorema De Pitagoras");
+    
+    }
+    }
+}
