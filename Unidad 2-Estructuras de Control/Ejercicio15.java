92727daa8fca31451bd2c881b754f6b4750fb0f8
ddgant <60008674+ddgant@users.noreply.github.com>
2020-02-09 17:41:41 -0600

    Update Ejercicio15.java

--- Unidad 2-Estructuras de Control/Ejercicio15.java
+++ Unidad 2-Estructuras de Control/Ejercicio15.java
@@ -17,37 +17,63 @@ public class Main{
 
    //Solicita y agarra el dato ingresado por el usuario para fecha 1
    System.out.println("Ingrese la 1er fecha");
-   System.out.println("Ingrese el dia(1-31)");
-   diaUno = entrada.nextInt();
-   System.out.println("Ingrese el mes(1-12)");
-   mesUno = entrada.nextInt();
+   
+   do{
+    System.out.println("Ingrese el dia(1-31)");
+    diaUno = entrada.nextInt();
+   }while(diaUno>0 || diaUno<32);
+   
+   do{
+    System.out.println("Ingrese el mes(1-12)");
+    mesUno = entrada.nextInt();
+   }while(mesUno>0 || mesUno<13);
+
    System.out.println("Ingrese el año");
    anioUno = entrada.nextInt();
 
    //Solicita y agarra el dato ingresado por el usuario para fecha 2
    System.out.println("Ingrese la 2da fecha");
+
+   do{
    System.out.println("Ingrese el dia(1-31)");
    diaDos = entrada.nextInt();
-   System.out.println("Ingrese el mes(1-12)");
-   mesDos = entrada.nextInt();
+   }while(diaDos>0 || diaDos<32);
+
+   do{
+    System.out.println("Ingrese el mes(1-12)");
+    mesDos = entrada.nextInt();
+   }while(mesDos>0 || mesDos<13);
+
    System.out.println("Ingrese el año");
    anioDos = entrada.nextInt();
 
    //Comprobacion de fecha por orden(Año, mes, dia)
    if(anioUno > anioDos){
-       System.out.println("El año mayor es el dia " + diaUno + ", del mes " + mesUno + ", del año " + anioUno);
+       System.out.println(diaUno); 
+       System.out.println(mesUno);
+       System.out.println(anioUno);
    } else if(anioDos > anioUno){
-       System.out.println("El año mayor es el dia " + diaDos + ", del mes " + mesDos + ", del año " + anioDos);
+      System.out.println(diaDos);
+      System.out.println(mesDos);
+      System.out.println(anioDos);;
    } else  if(anioUno == anioDos){
        if(mesUno > mesDos){
-           System.out.println("El año mayor es el dia " + diaUno + ", del mes " + mesUno + ", del año " + anioUno);
+          System.out.println(diaUno); 
+          System.out.println(mesUno);
+          System.out.println(anioUno);
        } else if(mesDos > mesUno){
-           System.out.println("El año mayor es el dia " + diaDos + ", del mes " + mesDos + ", del año " + anioDos);
+          System.out.println(diaDos);
+          System.out.println(mesDos);
+          System.out.println(anioDos);
        } else if(mesUno == mesDos){
            if(diaUno > diaDos){
-               System.out.println("El año mayor es el dia " + diaUno + ", del mes " + mesUno + ", del año " + anioUno);
+              System.out.println(diaUno); 
+              System.out.println(mesUno);
+              System.out.println(anioUno);
            } else if (diaDos > diaUno){
-               System.out.println("El año mayor es el dia " + diaDos + ", del mes " + mesDos + ", del año " + anioDos);
+               System.out.println(diaDos);
+               System.out.println(mesDos);
+               System.out.println(anioDos);
            } else if (diaUno == diaDos){
                System.out.println("Las fechas son iguales");
            }
