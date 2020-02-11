a2d72bdc53e330ea4da4d21a349eb09a3616f76e
ddgant <60008674+ddgant@users.noreply.github.com>
2020-02-10 20:30:43 -0600

    Update Ejercicio15.java

--- Unidad 2-Estructuras de Control/Ejercicio15.java
+++ Unidad 2-Estructuras de Control/Ejercicio15.java
@@ -8,46 +8,31 @@
 import java.util.Scanner;
 
 public class Main{
- public static void main(String arg[]){
+ public static void main(final String arg[]) {
 
-   //Definir variables
-   Scanner entrada = new Scanner(System.in); 
-   int diaUno = 0, mesUno = 0, anioUno = 0;
-   int diaDos = 0, mesDos = 0, anioDos = 0;
+   // Definir variables
+   final Scanner entrada = new Scanner(System.in);
+   int diaUno = 0, mesUno = 0, anioUno = 0, diaDos = 0, mesDos = 0, anioDos = 0;
 
    //Solicita y agarra el dato ingresado por el usuario para fecha 1
    System.out.println("Ingrese la 1er fecha");
-   
-   do{
-    System.out.println("Ingrese el dia(1-31)");
-    diaUno = entrada.nextInt();
-   }while(diaUno>0 && diaUno<32);
-   
-   do{
-    System.out.println("Ingrese el mes(1-12)");
-    mesUno = entrada.nextInt();
-   }while(mesUno>0 && mesUno<13);
-
+   System.out.println("Ingrese el día(1-31)");
+   diaUno = entrada.nextInt();
+   System.out.println("Ingrese el mes(1-12)");
+   mesUno = entrada.nextInt();
    System.out.println("Ingrese el año");
    anioUno = entrada.nextInt();
 
    //Solicita y agarra el dato ingresado por el usuario para fecha 2
    System.out.println("Ingrese la 2da fecha");
-
-   do{
-   System.out.println("Ingrese el dia(1-31)");
+   System.out.println("Ingrese el día(1-31)");
    diaDos = entrada.nextInt();
-   }while(diaDos>0 && diaDos<32);
-
-   do{
-    System.out.println("Ingrese el mes(1-12)");
-    mesDos = entrada.nextInt();
-   }while(mesDos>0 && mesDos<13);
-
+   System.out.println("Ingrese el mes(1-12)");
+   mesDos = entrada.nextInt();
    System.out.println("Ingrese el año");
    anioDos = entrada.nextInt();
 
-   //Comprobacion de fecha por orden(Año, mes, dia)
+   //Comprobacion de fecha por orden(año, mes, día)
    if(anioUno > anioDos){
        System.out.println(diaUno); 
        System.out.println(mesUno);
