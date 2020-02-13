408d538cf154fb24b6c98b68927ce462d938d38f
GildardoMaldonado <60483141+GildardoMaldonado@users.noreply.github.com>
2020-02-12 22:12:32 -0600

    Gil
    Cambios

--- Unidad 2-Estructuras de Control/Ejercicio17.py
+++ Unidad 2-Estructuras de Control/Ejercicio17.py
@@ -1,26 +1,32 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb  5 20:52:27 2020
+#Ejercicio 17
+#Escribir un programa que dados dos ángulos positivos (valores enteros),
+#determine lo siguiente:
+#a. Si los ángulos son iguales
+#b. Si los ángulos son menores a 90, imprimir “ángulos agudos”
+#c. Si los ángulos son iguales a 90, imprimir “ángulos rectos”
+#d. Si los ángulos son mayores a 90, imprimir “ángulos obtusos”
+#e. Si los ángulos son iguales a 180, imprimir “ángulos llanos”
+#f. Si los ángulos son mayores a 180, imprimir “ángulos cóncavos”
 
-@author: gilox
-"""
-#Entradas: Dos ángulos
-#Salidas: Clasificación de los ángulos según sus valores
-ang1 = int(input('Dáme un ángulo: '))
-ang2 = int(input('Dáme un segundo ángulo: '))
-if ang1 == ang2 and ang1 and ang2 != 180 and ang1 and ang2 != 90:
+#Autor: Gildardo Maldonado (Equipo 5)
+#Entradas: Ángulos en valores enteros
+#Salidas: Impresión del tipo de ángulo
+
+#Se solicitan ambos angulos
+aAng1 = int(input('Dáme un ángulo: '))
+aAng2 = int(input('Dáme un segundo ángulo: '))
+#Se realizan las comparaciones y se imprime seg�n su clasificaci�n
+if aAng1 == aAng2 and aAng1 !=180 and aAng2 != 180 and aAng1 != 90 and aAng2 != 90:
     print('ángulos iguales')
-elif ang1 != ang2:
-    print('ángulos diferentes')
-elif ang1 and ang2 < 90:
+elif aAng1 < 90 and aAng2 < 90:
     print('ángulos agudos')
-elif ang1 and ang2 == 90:
+elif aAng1 == 90 and aAng2 == 90:
     print('ángulos rectos')
-elif ang1 and ang2 > 90 and ang1 and ang2 < 180:
+elif aAng1 > 90 and aAng2 > 90 and aAng1 < 180 and aAng2 < 180:
     print('ángulos obtusos')
-elif ang1 and ang2 == 180:
+elif aAng1 == 180 and aAng2 == 180:
     print('ángulos llanos')
 else:
-    ang1 and ang2 > 180
+    aAng1 > 180 and aAng2 > 180
     print('ángulos cóncavos')
     
--- Unidad 2-Estructuras de Control/Ejercicio23.py
+++ Unidad 2-Estructuras de Control/Ejercicio23.py
@@ -1,22 +1,26 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb  5 21:04:46 2020
+#Ejercicio 23
+#Determinar la serie siguiente:
+#a. e^x = 1 + x + x^2/2! + x^3/3! + … + x^n/n!
 
-@author: gilox
-"""
-#Entrada: las entradas serán n y x
-#Salidas: El valor de e^x
+#Autor: Gildardo Maldonado (Equipo 5)
+#Entrada: Número flotante x y entero n
+#Salidas: resultado de la sustitución de los números en la fórmula 
 print('Calcularemos el valor de e^x = 1 + x + x^2/2!...x^n/n!')
-x = int(input('dame el valor de x'))
-n = int(input('dame el valor de n'))
-e = 1
-i = 0
-f = 1
-while i < n:
-    i = i + 1
-    f = f * i
-    y = (x**i)/f
-    e = e + y
+#Se solicita los valores para x y n
+aValorx = float(input('Dame el valor de x: '))
+aValorn = int(input('Dame el valor de n (que sea entero): '))
+#La formula lleva un 1 desde el principio
+aValordee = 1
+#Contador
+aContadori = 0
+#Variable de factorial
+aFactorial = 1
 
-print('el valor de e^x es', e)
+#Ciclo para resolución del problema
+while aContadori < aValorn:
+    aContadori = aContadori + 1
+    aFactorial = aFactorial * aContadori
+    aValordee = aValordee + (aValorx**aContadori)/(aFactorial)
+#Imprime el resultado
+print(aValordee)
   
