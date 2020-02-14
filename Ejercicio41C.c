14eec6d402d960111133b82e8368c2aad1da9824
JorgeLimonF <59981929+JorgeLimonF@users.noreply.github.com>
2020-02-14 00:00:52 -0600

    Update Ejercicio41C.c

--- Ejercicio41C.c
+++ Ejercicio41C.c
@@ -3,28 +3,25 @@
 int main() {
 	int menor = 0, mayor = 0, n = 0, numero = 0, x = 0;
 	
-	printf("¿Cuántos números va a usar?");
-		scanf("%d",&n);
-		for (x = 0; x < n; x++){
-			printf("Ingrese un número: ");
-				scanf("%d",&numero);
-				if (x == 0){
-					mayor = numero;
-					menor = numero;
-				}
-				if (numero > mayor){
-					mayor = numero;
-				}
-				else
-					if (numero < menor){
-						menor = numero;
-					}
-				
+	scanf("%d",&n); /*AquÃ­ se da la cantidad de nÃºmeros que se va a evaluar*/
+	for (x = 0; x < n; x++){
+		/*AquÃ­ se van dando los nÃºmeros a evaluarse*/
+		scanf("%d",&numero);
+		if (x == 0){
+			mayor = numero;
+			menor = numero;
 		}
-		printf("El número mayor es: ");
-		printf("%d%", mayor);
-		printf(" y el menor es: ");
-		printf("%d%", menor);
+		if (numero > mayor){
+			mayor = numero;
+		}
+		else
+			if (numero < menor){
+				menor = numero;
+			}
+		
+}
+printf("%d%", mayor);
+printf("%d", menor);
 	
 }
 
