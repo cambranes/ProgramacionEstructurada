8c6676f19f206ea42186fc946cb0903d2d6d6094
PamelaCanul <59714317+PamelaCanul@users.noreply.github.com>
2020-02-19 22:13:15 -0600

    Pamela Canul

---
+++ Ejercicio1.c
@@ -0,0 +1,28 @@
+/* Ejercicio #1
+Escribe un programa que reciba un número y te diga si ese número es par o
+impar.
+- Pamela C. Canul Chacón (Equipo 'about:blank') */
+#include <stdio.h>
+int main()
+{
+	//Entradas 
+	int num;
+	int esPar;
+	
+	//Solicitud de ingresar datos
+	scanf("%d",&num);
+	
+	//procedimiento
+	if (num%2 == 0) //Se compara con el modulo del número y 2 para saber si el número es par
+	{
+		esPar = 1; //La variable esPar toma valor positivo si el resultado de la división es cero
+	}
+	else{
+		esPar = 0; //La variable esPar toma valor negativo si el resultado de la división no es cero
+	}
+	
+	//Resultados
+	printf("%d",esPar);
+	
+	return 0; 
+}
