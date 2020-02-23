92401eb8f4a7d77cb5dd77dde8726c6cdb46322d
Joshua Immanuel Meza Magaña <56287951+JoshuaMeza@users.noreply.github.com>
2020-02-23 15:11:43 -0600

    PYDOC WORKS

--- Unidad 3-Funciones/Ejercicio43.py
+++ Unidad 3-Funciones/Ejercicio43.py
@@ -1,18 +1,18 @@
-'''
+"""
 Author Joshua Immanuel Meza Magana
 Version 1.0
 Program who count how many positive and negative numbers are on N numbers, 0 break the count.
-'''
+"""
 
 #Functions
 def specifyNumbers():
-    '''
+    """
     It gets the N numbers that we will count.
         Args:
             _N (int): Value of the number of numbers
         Returns:
             The N numbers that will be counted
-    '''
+    """
     _N=0
 
     _N=int(input())
@@ -24,7 +24,7 @@ def specifyNumbers():
     return _N
 
 def inputNumbers(_N):
-    '''
+    """
     Get a vector of the numbers that we will compare.
         Args:
             _N (int): Number of numbers
@@ -33,7 +33,7 @@ def inputNumbers(_N):
             con (int): Counter
         Returns:
             Values of the list of numbers
-    '''
+    """
     _listNumbers=[]
     num=0
     con=0
@@ -50,7 +50,7 @@ def inputNumbers(_N):
     return _listNumbers
 
 def pResult(_N,_listNumbers):
-    '''
+    """
     It calculates the amount of positive numbers.
         Args:
             _N (int): Number of numbers
@@ -58,7 +58,7 @@ def pResult(_N,_listNumbers):
             _pos (int): Ammount of positive numbers
         Returns:
             Ammount of positive numbers
-    '''
+    """
     _pos=0
     
     for x in _listNumbers:
@@ -68,7 +68,7 @@ def pResult(_N,_listNumbers):
     return _pos
 
 def nResult(_N,_listNumbers):
-    '''
+    """
     It calculates the amount of negative numbers.
         Args:
             _N (int): Number of numbers
@@ -76,7 +76,7 @@ def nResult(_N,_listNumbers):
             _neg (int): Ammount of negative numbers
         Returns:
             Ammount of negative numbers
-    '''
+    """
     _neg=0
 
     for x in _listNumbers:
@@ -87,43 +87,47 @@ def nResult(_N,_listNumbers):
 
 
 def printResults(_pos,_neg):
-    '''
+    """
     It print the amount of positive and negative numbers
         Args:
             _pos (int): Copy of the value of the positive numbers
             _neg (int): Copy of the value of the negative numbers
         Returns:
             Nothing
-    '''
+    """
     print(_pos)
     print(_neg)
 
-#Input
-'''
-It recives the N amount of numbers, and then it reads them.
-    Args:
-        N (int): Number of numbers
-        listNumbers (list): List of numbers
-'''
-N=0
-listNumbers=[]
-N=specifyNumbers()
-listNumbers.extend(inputNumbers(N))
+def main():
+    #Input
+    """
+    It recives the N amount of numbers, and then it reads them.
+        Args:
+            N (int): Number of numbers
+            listNumbers (list): List of numbers
+    """
+    N=0
+    listNumbers=[]
+    N=specifyNumbers()
+    listNumbers.extend(inputNumbers(N))
 
-#Process
-'''
-Calculate how many positive and negative numbers are on the N numbers.
-    Args:
-        pos (int): Ammount of positive numbers
-        neg (int): Amount of negative numbers
-        N (int): Number of numbers
-        listNumbers (list): List of numbers
-'''
-pos=pResult(N,listNumbers)
-neg=nResult(N,listNumbers)
+    #Process
+    """
+    Calculate how many positive and negative numbers are on the N numbers.
+        Args:
+            pos (int): Ammount of positive numbers
+            neg (int): Amount of negative numbers
+            N (int): Number of numbers
+            listNumbers (list): List of numbers
+    """
+    pos=pResult(N,listNumbers)
+    neg=nResult(N,listNumbers)
 
-#Output
-'''
-Print the results.
-'''
-printResults(pos,neg)
+    #Output
+    """
+    Print the results.
+    """
+    printResults(pos,neg)
+
+if __name__=="__main__":
+    main()
--- Unidad 3-Funciones/Ejercicio48.py
+++ Unidad 3-Funciones/Ejercicio48.py
@@ -1,18 +1,19 @@
-'''
+"""
 Author Joshua Immanuel Meza Magana
 Version 1.0
-Program who add a little amount of money to the salary of a worker depending on the time he has worked on the company.
-'''
+Program who add a little amount of money to the salary of a worker 
+depending on the time he has worked on the company.
+"""
 
 #Functions
 def readAge():
-    '''
+    """
     It reads the age of the worker.
-        Args:
-            _age (int): Value of the age of the worker 
-        Returns:
-            The age of the worker
-    '''
+    Args:
+        _age (Int): Value of the age of the worker 
+    Returns:
+        The age of the worker
+    """
     _age=0
 
     _age=int(input())
@@ -24,13 +25,13 @@ def readAge():
     return _age
 
 def readMoney():
-    '''
+    """
     It reads the salary of the worker.
-        Args:
-            _money (float): Salary of the worker 
-        Returns:
-            The salary of the worker
-    '''
+    Args:
+        _money (Float): Salary of the worker 
+    Returns:
+        The salary of the worker
+    """
     _money=0
     
     _money=float(input())
@@ -42,15 +43,15 @@ def readMoney():
     return _money
 
 def calculateExtra(_age,_money):
-    '''
+    """
     It calculates the total new amount of money.
-        Args:
-            _age (int): Age of the worker
-            _money (float): Normal amount of money
-            _total (float): New total amount of money
-        Returns:
-            The new total amount of money
-    '''
+    Args:
+        _age (Int): Age of the worker
+        _money (Float): Normal amount of money
+        _total (Float): New total amount of money
+    Returns:
+        The new total amount of money
+    """
     _total=float(0)
 
     if _age>=1 and _age<=3:
@@ -67,43 +68,47 @@ def calculateExtra(_age,_money):
     return _total
 
 def printResult(_total):
-    '''
+    """
     It prints the total of money.
-        Args:
-            _total (float): Total of money
-        Returns:
-            Nothing
-    '''
+    Args:
+        _total (Float): Total of money
+    Returns:
+        Nothing
+    """
     print(_total)
 
-#Input
-'''
-It receives the values.
+def main():
+    #Input
+    """
+    It receives the values.
     Args:
-        age (int): Age of the worker
-        money (float): Salary of the worker
-        total (float): New total amount of money
-'''
-age=0
-money=0
-total=0
-age=int(readAge())
-money=float(readMoney())
+        age (Int): Age of the worker
+        money (Float): Salary of the worker
+        total (Float): New total amount of money
+    """
+    age=0
+    money=0
+    total=0
+    age=int(readAge())
+    money=float(readMoney())
 
-#Process
-'''
-It calculates the new total.
+    #Process
+    """
+    It calculates the new total.
     Args:
-        age (int): Age of the worker
-        money (float): Salary of the worker
-        total (float): New total amount of money
-'''
-total=float(calculateExtra(age,money))
+        age (Int): Age of the worker
+        money (Float): Salary of the worker
+        total (Float): New total amount of money
+    """
+    total=float(calculateExtra(age,money))
 
-#Output
-'''
-It prints the result.
+    #Output
+    """
+    It prints the result.
     Args:
-        total (float): New total amount of money
-'''
-printResult(total)
+       total (Float): New total amount of money
+    """
+    printResult(total)
+
+if __name__=="__main__":
+    main()
