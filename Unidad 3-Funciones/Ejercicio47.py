571d19104bb54cc648c70758eb479dfd1ddece76
Apoquinto <RobertoCarlosLlanesMontero@gmail.com>
2020-02-24 01:52:30 -0600

    Add cosos

--- Unidad 3-Funciones/Ejercicio47.py
+++ Unidad 3-Funciones/Ejercicio47.py
@@ -1,6 +1,5 @@
 #----------------------------------Datos del problema-------------------------------------
-#Ejercicio #
-# Dados dos números enteros determinar su máximo común divisor.
+#Escribe elprogramaque lea datos de muchos empleados de una fábrica. De cada uno se leerá: Nombre, Edad, Sueldo por hora, Horas trabajadas en la semana  y  Porcentaje  de  impuestos  a  pagar.    El  pseudocódigo  calculará  e imprimirá el sueldo neto semanal de cada empleado, el cual es el sueldo ya con    los  impuestos  deducidos.  Se  debe  considerar  que  las  horas  extra (después de 40 horas semanales se pagan al doble).
 # Roberto Carlos Llanes Montero (Equipo 'about:blank')
 
 #----------------------------------Prototipo de funciones----------------------------------
@@ -42,7 +41,7 @@ def process(timeWork,salary,tax):
 		#Saca las horas extra
 		extra = int(timeWork) - 40
 		#Calcula el sueldo
-		return ((float(timeWork)*float(salary))+(float(extra)*float(salary)))*float(tax)
+		return ((float(timeWork)*float(salary))+(2*float(extra)*float(salary)))*float(tax)
 	else:
 		return (float(timeWork)*float(salary))*float(tax)	
 		
@@ -50,23 +49,23 @@ def printOut(finale):
 	print(finale)
 
 #----------------------------------------Proceso-------------------------------------------
-#Obtener el numero de empleados
-employees = getEmployees()
+def main():
+	#Obtener el numero de empleados
+	employees = getEmployees()
 
-#Pedir los datos de todos los empleados
-for employe in range(int(employees)):
+	#Pedir los datos de todos los empleados
+	for employe in range(int(employees)):
+		#Obtener datos del empleado 
+		name = getName()
+		age = getAge()
+		salary = getSalary()
+		timeWork = getTimeWork()
+		tax = getTax()
 
-	#Obtener datos del empleado 
-	name = getName()
-	age = getAge()
-	salary = getSalary()
-	timeWork = getTimeWork()
-	tax = getTax()
+		#Proceso
+		result = process(timeWork, salary, tax)
 
-	#Proceso
-	result = process(timeWork, salary, tax)
-
-	#Imprimir resultado
-	printOut(result)
+		#Imprimir resultado
+		printOut(result)
 	
-
+main()
