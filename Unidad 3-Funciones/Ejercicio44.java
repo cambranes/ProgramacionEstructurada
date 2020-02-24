f3a51d6da525fd9f27c7224d56bbc45a3741a4fc
ddgant <60008674+ddgant@users.noreply.github.com>
2020-02-24 02:00:55 -0600

    Update Ejercicio44.java

--- Unidad 3-Funciones/Ejercicio44.java
+++ Unidad 3-Funciones/Ejercicio44.java
@@ -1,8 +1,18 @@
+/*Autor: Diego De Gante Pérez
+*Entradas: dos numeros enteros a dividir num, num2
+*Salidas: El MCD de dos números con el algoritmo de Euclides
+*Proceso:
+a. Dividir el mayor de los dos números por el más pequeño
+b. A continuación dividir el divisor por el resto
+c. Continuar el proceso de dividir el último resto hasta que la división sea exacta.
+d. El último divisor es el MCD
+*/
 import java.util.Scanner;
 
 public class Main{
   
     public static void main(String[] args) {
+        
         //Entradas
         int num,num2;
         entradas(num,num2);
