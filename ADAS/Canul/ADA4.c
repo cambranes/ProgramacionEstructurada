b25a7eb69dc23bda68d0fd24c8fd15be66800c7a
PamelaCanul <59714317+PamelaCanul@users.noreply.github.com>
2020-03-25 14:35:17 -0600

    Update ADA4.c

--- ADAS/Canul/ADA4.c
+++ ADAS/Canul/ADA4.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define N 3
+#define N 8
 
-/*ADA 4
-Autor: Pamela Canul Chacón*/
 
 void asignarArreglo (int arr [][N]);
 void imprimirArreglo (int arr [][N]);
@@ -30,13 +28,13 @@ void recorrerMatriz4Descendente (int arr [][N]);
 int main () {
 	int arreglo[N][N];
 	time_t t; 
-	/* Initializes random number generator */
+	/* inicializacion del generador de numeros random*/
 	srand((unsigned) time(&t));
 	
 	asignarArreglo(arreglo);
 	imprimirArreglo(arreglo); 
 	recorrerDiagonalPrincipal(arreglo);
-	recorrerDiagonalInvertida(arreglo); 
+	recorrerDiagonalInvertida(arreglo);
 	recorrerMatriz12Ascendente(arreglo);
 	recorrerMatriz12Descendente(arreglo);
 	recorrerMatriz34Ascendente(arreglo);
@@ -57,17 +55,16 @@ int main () {
 	return(0);
 }
 
-
 /*asignar valores a la matriz*/
 void asignarArreglo(int arr[][N]){
 	for (int i = 0; i < N; i++){
 		for (int j = 0; j < N; j++){
-			arr[i][j] = rand() % 50;
+			arr[i][j] = rand() % 10;
 		}
 	}
 }
-
-	/*Imprimir la matriz */
+	
+/*Imprimir la matriz */	
 void imprimirArreglo (int arr[][N]){
 	for(int i = 0; i < N; i++) {
 		for(int j = 0; j < N; j++) {
@@ -75,30 +72,30 @@ void imprimirArreglo (int arr[][N]){
 		}
 		printf("\n");
 	}
+	printf("\n");
 }
 
-	/*Recorrer la diagonal principal */ 
+/*Recorrer la diagonal principal */ 
 void recorrerDiagonalPrincipal (int arr[][N]) { 
 	printf("Diagonal Principal\n");
 	for(int j = 0; j<N; j++) {
 		printf("%d,%d : %d ", j,j, arr[j][j]);
-		printf(" ");
+		printf("\n");
 	}
 	printf("\n");
 }
 
-/*Recorrer la diagonal invertida*/
+/*Recorrer la diagonal invertida*/ 
 void recorrerDiagonalInvertida (int arr[][N]) {
 	printf("Diagonal Invertida\n");
 	for (int i=0; i<N; i++) {
 		for (int j=(N-1-i); j<(N-i);j++){
 			printf("%d,%d :%d ", i,j,arr[i][j]);
-			printf(" ");
+			printf("\n");
 		}
 	}
 	printf("\n");
 }
-
 
 /*Recorrer las regiones 1 y 2 de la matriz de forma ASCENDENTE*/
 void recorrerMatriz12Ascendente (int arr[][N]){
@@ -110,16 +107,17 @@ void recorrerMatriz12Ascendente (int arr[][N]){
 		}
 		printf("\n");
 	}
-
+	printf("\n");	
 }
-	/*Recorrer las regiones 1 y 2 de la matriz de forma DESCENDENTE*/
+
+/*Recorrer las regiones 1 y 2 de la matriz de forma DESCENDENTE*/
 void recorrerMatriz12Descendente (int arr [][N]) {
 	printf("Region 1 y 2 Descendente\n");
 	for (int i=N-1;i>=0;i--){
 		for (int j=0; j<(N-i);j++){
 			printf("%d,%d :%d ", i,j,arr[i][j]);
-			printf(" ");
 		}
+		printf("\n");
 	}
 	printf("\n");
 }
@@ -131,11 +129,12 @@ void recorrerMatriz34Ascendente (int arr [][N]){
 		for(int j=N-1; j>=(N-1-i);j--) {
 			printf("%d,%d : %d ", i,j, arr[i][j]);
 		}
+		printf("\n");
 	}
 	printf("\n");
 }
 
-	/*Recorrer las regiones 3 y 4 de la matriz de forma DESCENDENTE*/
+/*Recorrer las regiones 3 y 4 de la matriz de forma DESCENDENTE*/
 void recorrerMatriz34Descendente (int arr [][N]){
 	printf("Region 3 y 4 Descendente\n");
 	for(int i=N-1; i>=0;i--) {
@@ -144,9 +143,10 @@ void recorrerMatriz34Descendente (int arr [][N]){
 		}
 		printf("\n");
 	}
+	printf("\n");
 }
-	
-	/*Recorrer las regiones 1 y 4 de la matriz de forma ASCENDENTE*/
+
+/*Recorrer las regiones 1 y 4 de la matriz de forma ASCENDENTE*/
 void recorrerMatriz14Ascendente (int arr [][N]){
 	printf("Region 1 y 4 Ascendente\n");
 	for(int i = 0; i < N; i++) {
@@ -155,30 +155,33 @@ void recorrerMatriz14Ascendente (int arr [][N]){
 		}
 		printf("\n");
 	}
+	printf("\n");
 }
-	
-	/*Recorrer las regiones 1 y 4 de la matriz de forma DESCENDENTE*/
+
+/*Recorrer las regiones 1 y 4 de la matriz de forma DESCENDENTE*/
 void recorrerMatriz14Descendente (int arr [][N]){
 	printf("Region 1 y 4 Descendente\n");
 	for(int i = (N-1); i>=0; i--) {
 		for(int j =0; j<=i; j++) {
 			printf("%d,%d : %d ", i,j, arr[i][j]);
+		}
+		printf("\n");
 	}
 	printf("\n");
-	}
 }
 
-	/*Recorrer las regiones 2 y 3 de la matriz de forma ASCENDENTE*/
+/*Recorrer las regiones 2 y 3 de la matriz de forma ASCENDENTE*/
 void recorrerMatriz23Ascendente (int arr [][N]) {
 	printf("Region 2 y 3 Ascendente\n");
 	for(int i = 0; i<N; i++) {
-		for(int j =i; j<=(N-1);j++) {
+		for(int j =i; j<=(N-1); j++) {
 			printf("%d,%d : %d ", i,j, arr[i][j]);
 		}
 		printf("\n");
 	}	
+	printf("\n");
 }
-
+	
 /*Recorrer las regiones 2 y 3 de la matriz de forma DESCENDENTE*/
 void recorrerMatriz23Descendente (int arr [][N]){
 	printf("Region 2 y 3 Descendente\n");
@@ -189,148 +192,125 @@ void recorrerMatriz23Descendente (int arr [][N]){
 		printf("\n");
 	}
 }
-	
-	/*Recorrer la region 1 de forma ASCENDENTE*/
+
+/*Recorrer la region 1 de forma ASCENDENTE*/
 void recorrerMatriz1Ascendente (int arr [][N]){
 	printf("Region 1 Ascendente\n");
-	for(int i = 0; i < N; i++) {
-		if (i!=0 && i!=2){
-			for (int j=0; j<=i; j++) {
-			printf("%d,%d : %d ", i,j, arr[i][j]);
+	/*Para imprimir la región se dividira en 2*/
+	for(int i=0; i<N; i++){
+		if(i<=N/2) {
+			for (int j=0;j<(1+i);j++){
+				printf("%d,%d : %d ", i,j, arr[i][j]);
 			}
 		}
 		else {
-			int j=0;
-			printf("%d,%d : %d ", i,j, arr[i][j]);
+			for(int j=0;j<=(N-1-i);j++){
+				printf("%d,%d : %d ", i,j, arr[i][j]);
 			}
-		
-		printf("\n");
 		}
+		printf("\n");
+	}
+	printf("\n");
 }
 
-	
-	/*Recorrer la region 1 de forma DESCENDENTE*/
+/*Recorrer la region 1 de forma DESCENDENTE*/
 void recorrerMatriz1Descendente (int arr [][N]){
 	printf("Region 1 Descendente\n");
-	for(int i =(N-1); i>=0; i--) {
-		if (i!=0 && i!=2){
-			for (int j=0; j<=i; j++) {
+	for(int i=(N-1); i>=0; i--){
+		if(i<=N/2) {
+			for (int j=0;j<(1+i);j++){
 				printf("%d,%d : %d ", i,j, arr[i][j]);
 			}
 		}
 		else {
-			int j=0;
-			printf("%d,%d : %d ", i,j, arr[i][j]);
+			for(int j=0;j<=(N-1-i);j++){
+				printf("%d,%d : %d ", i,j, arr[i][j]);
+			}
 		}
-		
 		printf("\n");
 	}
+	printf("\n");
 }
-	
-	/*Recorrer la region 2 de forma ASCENDENTE*/
+/*Recorrer la region 2 de forma ASCENDENTE*/
 void recorrerMatriz2Ascendente (int arr [][N]) {
 	printf("Region 2 Ascendente\n");
-	for(int i = 0; i<=1; i++) {
-		if (i==0){
-			for(int j =0; j < N; j++) {
-				printf("%d,%d : %d ", i,j, arr[i][j]);
-				}
-			printf("\n");
-		}
-		else {
-			int j=1;
+	for(int i = 0; i<=N/2; i++) {
+		for(int j=i; j<=(N-1)-i;j++){
 			printf("%d,%d : %d ", i,j, arr[i][j]);
 		}
+		printf("\n");
 	}
+	printf("\n");
 }
-
 
 /*Recorrer la region 2 de forma DESCENDENTE*/
 void recorrerMatriz2Descendente (int arr [][N]) {
 	printf("Region 2 Descendente\n");
-	for(int i =(N-2); i>=0; i--) {
-		if (i==0){
-			for(int j =0; j < N; j++) {
-				printf("%d,%d : %d ", i,j, arr[i][j]);
-			}
-			printf("\n");
-		}
-		else {
-			int j=1;
+	for(int i =N/2; i>=0; i--) {
+		for(int j=i; j<=(N-1)-i;j++){
 			printf("%d,%d : %d ", i,j, arr[i][j]);
-			printf("\n");
 		}
+		printf("\n");
 	}
+	printf("\n");
 }
-
+	
 /*Recorrer la region 3 de forma ASCENDENTE*/
 void recorrerMatriz3Ascendente (int arr [][N]) {
 	printf("Region 3 Ascendente\n");
-	for(int i = 0; i < N; i++) {
-		if (i!=0 && i!=2){
-			for (int j=(N-1); j>=(N-1-i); j--) {
+	for(int i = 0; i <(N-1)/2; i++) {
+		for(int j=(N-1-i);j<=N-1;j++){
 			printf("%d,%d : %d ", i,j, arr[i][j]);
-			}
 		}
-		else {
-			int j=2;
-			printf("%d,%d : %d ", i,j, arr[i][j]);
-			}
 		printf("\n");
 	}
+	for(int i=(N-1)/2;i<=N-1;i++){
+		for(int j=i;j<=N-1;j++){
+			printf("%d,%d : %d ", i,j, arr[i][j]);
+		}
+	printf("\n");
+	}
+	printf("\n");
 }
-
-
+	
+	
 /*Recorrer la region 3 de forma DESCENDENTE*/
 void recorrerMatriz3Descendente (int arr [][N]) {
 	printf("Region 3 Descendente\n");
-	for(int i =(N-1); i>=0; i--) {
-		if (i!=0 && i!=2){
-			for (int j=(N-1); j>=(N-1-i); j--) {
+	for (int i=(N-1); i>=0;i--){
+		if (i<=N/2){
+			for(int j=(N-1)-i;j<N;j++){
 				printf("%d,%d : %d ", i,j, arr[i][j]);
 			}
 		}
 		else {
-			int j=2;
-			printf("%d,%d : %d ", i,j, arr[i][j]);
+			for(int j=i; j<N;j++){
+				printf("%d,%d : %d ", i,j, arr[i][j]);
+			}
 		}
-		
 		printf("\n");
 	}
+	printf("\n");
 }
 
 /*Recorrer la region 4 de forma ASCENDENTE*/
 void recorrerMatriz4Ascendente (int arr [][N]){
 	printf("Region 4 Ascendente\n");
-	for(int i = 0; i < N; i++) {
-		if (i==2){
-			for(int j = 0; j < (1+i); j++) {
-				printf("%d,%d : %d ", i,j, arr[i][j]);
-			}
-		printf("\n");
-		}
-		else if (i==1){
-			int j=1;
+	for(int i =N/2; i < N; i++) {
+		for(int j=(N-1)-i; j<=i;j++){
 			printf("%d,%d : %d ", i,j, arr[i][j]);
-			printf("\n");
 		}
+		printf("\n");
 	}	
 }
-	
-	/*Recorrer la region 4 de forma DESCENDENTE*/
+
+/*Recorrer la region 4 de forma DESCENDENTE*/
 void recorrerMatriz4Descendente (int arr [][N]) {
 	printf("Region 4 Descendente\n");
-	for(int i = (N-1); i>=0; i--) {
-		if (i==2){
-			for(int j = 0; j <=i; j++) {
-				printf("%d,%d : %d ", i,j, arr[i][j]);
-			}
-			printf("\n");
-		}
-		else if (i==1){
-			int j=1;
+	for(int i =(N-1); i>=N/2;i--){
+		for(int j=(N-1)-i;j<=i;j++){
 			printf("%d,%d : %d ", i,j, arr[i][j]);
-			printf("\n");
 		}
-	}	
+		printf("\n");
+	}
 }
