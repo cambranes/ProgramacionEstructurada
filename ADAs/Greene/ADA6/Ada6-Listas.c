b6e039fff741591ff6576341e6a07f4ae0dcbe74
CarlosGreene <59940440+CarlosGreene@users.noreply.github.com>
2020-04-16 18:55:06 -0500

    Listas

--- ADAs/Greene/ADA6/Ada6-Listas.c
+++ ADAs/Greene/ADA6/Ada6-Listas.c
@@ -1,29 +1,138 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct animal
 {
-    char especie[15];
-    char raza[15];
-    char sexo[6];
     int edad;
-    float peso;
-} animal;
+    struct animal* siguiente;
+}Animal;
+typedef Animal *AnimalPtr;
 
-void insertarOrdenado(animal *);
-void borrar(animal);
-void imprimir(animal);
-void recorrerLista(animal);
+void insertarOrdenado(AnimalPtr*, int);
+AnimalPtr borrar(AnimalPtr, int);
+void imprimir(AnimalPtr);
+void menu();
 
 int main()
 {
-    animal _animal;
-    scanf("%s", _animal.especie);
-    scanf("%s", _animal.raza);
-    scanf("%s", _animal.sexo);
-    scanf("%d", _animal.edad);
-    scanf("%f", _animal.peso);
+    AnimalPtr lista=NULL;
+    int _edad, opcion; 
+    bool aux=false;
 
+    do
+    {
+        menu();
+        scanf("%d", &opcion);
 
+        switch (opcion)
+        {
+        case 1:
+            scanf("%d", &_edad);
+            insertarOrdenado(&lista, _edad);
+        break;
+
+        case 2:
+            scanf("%d", &_edad);
+            lista = borrar(lista, _edad);
+        break;
+        
+        case 3:
+            imprimir(lista);
+        break;
+
+        default:
+            aux=true;
+            break;
+        }
+        
+    } while (aux==false);
     return 0;
 }
 
+void menu()
+{
+    printf("Menú\n");
+    printf("Elige uno de las opciones de acuerdo a su numero para realizar una accion\n");
+    printf("1.\tInsertar un elemento\n");
+    printf("2.\tBorrar un elemento\n");
+    printf("3.\tImprimir la lista\n");
+    printf("Para cerrar el programa preciona otro botón");
+}
+
+void insertarOrdenado(AnimalPtr *sPtr, int valor)
+{
+    AnimalPtr nuevo;
+    AnimalPtr anterior;
+    AnimalPtr actual;
+    
+    nuevo = (Animal*)malloc(sizeof(Animal));
+    if (nuevo!=NULL)
+    {
+        nuevo->edad=valor;
+        nuevo->siguiente= NULL;
+
+        anterior=NULL;
+        actual=*sPtr;
+   
+        while (actual!=NULL && valor>actual->edad)
+        {
+            anterior=actual;
+            actual=actual->siguiente;
+        }
+
+        if (anterior==NULL)
+        {
+            nuevo->siguiente=*sPtr;
+            *sPtr=nuevo;
+        }
+        else
+        {
+            anterior->siguiente=nuevo;
+            nuevo->siguiente=actual;
+        }
+    }
+    else
+    {
+        printf("Sorry men");
+    }
+    
+}
+
+AnimalPtr borrar(AnimalPtr actual, int valor)
+{
+    AnimalPtr paraBorrar = NULL;
+    if (actual!=NULL)
+    {
+        if (actual->edad==valor)
+        {
+            paraBorrar=actual;
+            actual=actual->siguiente;
+            free(paraBorrar);
+        }
+        else
+        {
+            actual->siguiente=borrar(actual->siguiente, valor);
+        }        
+    }
+    return actual;
+}
+
+void imprimir(AnimalPtr actual)
+{
+    if (actual==NULL)
+    {
+        printf("Sorry men, no hay elementos");
+    }
+    else
+    {
+        printf("Imprimir lista");
+        while (actual!=NULL)
+        {
+            printf("%s", actual->edad);
+            actual=actual->siguiente;
+        }
+        printf("\n");
+    }
+    
+}
